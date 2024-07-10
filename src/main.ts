d310daf90a636ff5a0212ea4d88a1b0cf98524a2
kimizuy <kimizuyamasaki@gmail.com>
2024-07-10 16:42:28 -0700

    use app.whenReady()

--- src/main.ts
+++ src/main.ts
@@ -32,11 +32,6 @@ const createWindow = (): void => {
   }
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
-
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
@@ -46,18 +41,21 @@ app.on("window-all-closed", () => {
   }
 });
 
-app.on("activate", () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+app.whenReady().then(() => {
+  createWindow();
+
+  Object.entries(ipcMainHandlers).forEach(([key, value]) => {
+    ipcMain.handle(key, value);
+  });
+
+  app.on("activate", () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
-app.whenReady().then(() => {
-  Object.entries(ipcMainHandlers).forEach(([key, value]) => {
-    ipcMain.handle(key, value);
-  });
-});
