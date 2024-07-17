7d0683e3184063831a7331bf033e9427664b2bbf
kimizuy <kimizuyamasaki@gmail.com>
2024-07-17 16:01:11 -0700

    add dbCredentials for drizzle-kit push

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,4 +3,7 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./src/schema.ts",
   dialect: "sqlite",
+  dbCredentials: {
+    url: "database/database.db",
+  },
 });
