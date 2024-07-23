b1349d82e6151adf85b4399202a63795915d5c51
kimizuy <kimizuyamasaki@gmail.com>
2024-07-22 22:55:23 -0700

    update binary path

--- app/src/ipc-main-listeners/index.tsx
+++ app/src/ipc-main-listeners/index.tsx
@@ -19,7 +19,7 @@ export const execFileAsync = promisify(execFile);
 export const binaryPath = (name: string): string => {
   const isDev = process.env.NODE_ENV === "development";
   const _binaryPath = path.resolve(__dirname, "../../../ml/dist", name);
-  return isDev ? _binaryPath : path.join(process.resourcesPath, _binaryPath);
+  return isDev ? _binaryPath : path.join(process.resourcesPath, "dist", name);
 };
 
 export type IpcMainListener = Parameters<typeof ipcMain.handle>[1];
