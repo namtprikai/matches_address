4294459a0c632759af636fcfc7f7a402d9986ed8
kimizuy <kimizuyamasaki@gmail.com>
2024-07-29 21:42:28 -0700

    github protection test2

--- app/src/pages/home/index.tsx
+++ app/src/pages/home/index.tsx
@@ -4,7 +4,7 @@ import { type FormProps } from "react-router-dom";
 
 export function Home(): JSX.Element {
   const [names, setNames] = useState<string[] | null>(null);
-  console.log("github protection test");
+  console.log("github protection test2");
 
   const updateNames = async (): Promise<void> => {
     const result = await window.ipcRenderer.invoke("getNames");
