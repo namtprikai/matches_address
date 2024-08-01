8a497b065c284c7507feed186b473ad351f8822a
ksyunnnn <is.syunsukekobashi@gmail.com>
2024-08-01 09:40:27 +0900

    ファイル名をケバブに🍖

--- app/src/pages/layout.tsx
+++ app/src/pages/layout.tsx
@@ -1,6 +1,6 @@
 import { makeStyles, tokens } from "@fluentui/react-components";
 import { Outlet } from "react-router-dom";
-import { Sidebar } from "../components/Sidebar";
+import { Sidebar } from "../components/sidebar";
 
 const useStyles = makeStyles({
   root: {
