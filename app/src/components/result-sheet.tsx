76a8874c34cdd8f554f32b68b8410e60e564b481
ksyunnnn <is.syunsukekobashi@gmail.com>
2024-08-01 17:06:59 +0900

    フェッチ場所追加

--- app/src/components/result-sheet.tsx
+++ app/src/components/result-sheet.tsx
@@ -1,9 +1,20 @@
+import { useEffect } from "react";
 import { type result_sheets } from "../schema";
+import { Button } from "./button";
 
 type Props = {
   resultsheet: typeof result_sheets.$inferSelect;
 };
 
 export const Resultsheet = ({ resultsheet }: Props): JSX.Element => {
-  return <div>Resultsheet - {resultsheet.title}</div>;
+
+  useEffect(()=>{
+    // todo: fetch
+  },[resultsheet])
+
+  return (
+    <div>
+      <Button appearance="primary">保存</Button>
+    </div>
+  );
 };
