506f59428df083959a2df4215bfedbab4cdd323c
WataruNishimura <wataru.n@chot-inc.com>
2024-08-02 20:00:01 +0900

    fix: CSSPropertiesからuseStylesを使った記述に変更

--- app/src/components/table-workbook.tsx
+++ app/src/components/table-workbook.tsx
@@ -1,4 +1,4 @@
-import { type CSSProperties, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Table,
   TableBody,
@@ -8,6 +8,8 @@ import {
   TableRow,
   Link as FUILink,
   tokens,
+  makeStyles,
+  mergeClasses,
 } from "@fluentui/react-components";
 import dayjs from "dayjs";
 import { Link } from "react-router-dom";
@@ -15,35 +17,31 @@ import { type workbooks } from "../schema";
 
 type Workbook = typeof workbooks.$inferSelect;
 
-const StyleOfUpdatedAtHeaderCell: CSSProperties = {
-  tableLayout: "fixed",
-  width: "140px",
-};
-
-const StyleOfUpdatedAtCell: CSSProperties = {
-  fontSize: "12px",
-};
-
-const StyleOfCreatedAtHeaderCell: CSSProperties = {
-  tableLayout: "fixed",
-  width: "140px",
-};
-
-const StyleOfCreatedAtCell: CSSProperties = {
-  fontSize: "12px",
-};
-
-const StyleOfTableHeader: CSSProperties = {
-  backgroundColor: tokens.colorNeutralBackground3,
-};
-
-const StyleOfTableHeaderRow: CSSProperties = {
-  border: "none",
-};
-
-const StyleOfTableHeaderCell: CSSProperties = {
-  fontWeight: 600,
-};
+const useStyles = makeStyles({
+  updatedAtHeaderCell: {
+    tableLayout: "fixed",
+    width: "140px",
+  },
+  updatedAtCell: {
+    fontSize: "12px",
+  },
+  createdAtHeaderCell: {
+    tableLayout: "fixed",
+    width: "140px",
+  },
+  createdAtCell: {
+    fontSize: "12px",
+  },
+  tableHeader: {
+    backgroundColor: tokens.colorNeutralBackground3,
+  },
+  tableHeaderRow: {
+    border: "none",
+  },
+  tableHeaderCell: {
+    fontWeight: 600,
+  },
+});
 
 export const TableWorkbook = (): JSX.Element => {
   const [data, setData] = useState<Workbook[]>([]);
@@ -56,24 +54,28 @@ export const TableWorkbook = (): JSX.Element => {
     fetchData().catch(console.error);
   }, []);
 
+  const styles = useStyles();
+
   return (
     <Table>
-      <TableHeader style={StyleOfTableHeader}>
-        <TableRow style={StyleOfTableHeaderRow}>
-          <TableHeaderCell style={StyleOfTableHeaderCell}>名前</TableHeaderCell>
+      <TableHeader className={styles.tableHeader}>
+        <TableRow className={styles.tableHeaderRow}>
+          <TableHeaderCell className={styles.tableHeaderCell}>
+            名前
+          </TableHeaderCell>
           <TableHeaderCell
-            style={{
-              ...StyleOfCreatedAtHeaderCell,
-              ...StyleOfTableHeaderCell,
-            }}
+            className={mergeClasses(
+              styles.createdAtHeaderCell,
+              styles.tableHeaderCell,
+            )}
           >
             作成日
           </TableHeaderCell>
           <TableHeaderCell
-            style={{
-              ...StyleOfUpdatedAtHeaderCell,
-              ...StyleOfTableHeaderCell,
-            }}
+            className={mergeClasses(
+              styles.updatedAtHeaderCell,
+              styles.tableHeaderCell,
+            )}
           >
             更新日
           </TableHeaderCell>
@@ -94,10 +96,10 @@ export const TableWorkbook = (): JSX.Element => {
                 </FUILink>
               </Link>
             </TableCell>
-            <TableCell style={StyleOfCreatedAtCell}>
+            <TableCell className={styles.createdAtCell}>
               {dayjs(item.created_at).format("YYYY/MM/DD HH:mm")}
             </TableCell>
-            <TableCell style={StyleOfUpdatedAtCell}>
+            <TableCell className={styles.updatedAtCell}>
               {dayjs(item.updated_at).format("YYYY/MM/DD HH:mm")}
             </TableCell>
           </TableRow>
