d3ebb2eab78df35c35bc04123154e000dc0a2bce
WataruNishimura <wataru.n@chot-inc.com>
2024-08-08 19:32:56 +0900

    feat: カラム１つのみに変更

--- app/src/components/pie-charts.tsx
+++ app/src/components/pie-charts.tsx
@@ -3,108 +3,60 @@ import {
   Legend as ReLegend,
   Pie as RePie,
   Cell as ReCell,
+  ResponsiveContainer,
 } from "recharts";
-import { tokens } from "@fluentui/react-components";
 import { type ChartAccepatbleType } from "../@types/charts";
 import { Label } from "../config/label-map";
+import { CHART_COLORS } from "../config/chart-colors";
 
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({
-  cx,
-  cy,
-  midAngle,
-  innerRadius,
-  outerRadius,
-  percent,
-  index,
-}: {
-  cx: number;
-  cy: number;
-  midAngle: number;
-  innerRadius: number;
-  outerRadius: number;
-  percent: number;
-  index: number;
-}): JSX.Element => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.3;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-  return (
-    <text
-      dominantBaseline="central"
-      fill="white"
-      fontWeight={tokens.fontWeightBold}
-      textAnchor={x > cx ? "start" : "end"}
-      x={x}
-      y={y}
-    >
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
-  );
-};
-
-const renderLegend = (props): JSX.Element => {
-  const { payload } = props;
-
-  console.log(payload[0]);
-
-  return (
-    <ul>
-      {payload.map((entry, index) => (
-        <li key={`item-${index}`}>{entry.value}</li>
-      ))}
-    </ul>
-  );
-};
+export interface PieChartProps<T> {
+  keyColumn: {
+    key: keyof T;
+    type: "string" | "number";
+    label: string;
+  };
+  column: {
+    key: keyof T;
+    type: "string" | "number";
+    label: string;
+  };
+  data: T[];
+}
 
 export const PieChart = <T extends ChartAccepatbleType>({
   keyColumn,
-  colmuns,
+  column,
   data,
-  colors,
-}: {
-  keyColumn: keyof T;
-  colmuns: (keyof T)[];
-  data: T[];
-  colors: {
-    [k in keyof T]?: string;
-  };
-}): JSX.Element => {
+}: PieChartProps<T>): JSX.Element => {
   const chartData = data.map((row) => {
-    const kvPairs = colmuns.map((column) => {
-      return [Label[column as keyof typeof Label], row[column as keyof T]];
-    });
     return {
-      name: row[keyColumn],
-      ...Object.fromEntries(kvPairs),
+      name: row[keyColumn.key],
+      [Label[column.key as keyof typeof Label]]: row[column.key],
     };
   });
   return (
-    <RePieChart height={400} width={400}>
-      {colmuns.map((column, index) => {
-        return (
-          <RePie
-            key={column as string}
-            cx="50%"
-            cy="50%"
-            data={chartData}
-            dataKey={Label[column as keyof typeof Label]}
-            fill={colors[column]}
-            innerRadius={index * 2 * 30}
-            label={renderCustomizedLabel}
-            labelLine={false}
-            nameKey="name"
-            outerRadius={(index + 1) * 2 * 30}
-            startAngle={0}
-          >
-            {chartData.map((entry, index) => {
-              return <ReCell key={index} fill={colors[column]} />;
-            })}
-          </RePie>
-        );
-      })}
-      <ReLegend content={renderLegend} />
-    </RePieChart>
+    <ResponsiveContainer height={400} width="100%">
+      <RePieChart height={400} width={400}>
+        <RePie
+          cx="50%"
+          cy="50%"
+          data={chartData}
+          dataKey={Label[column.key as keyof typeof Label]}
+          labelLine={false}
+          nameKey="name"
+          startAngle={0}
+        >
+          {chartData.map((_, index) => {
+            return (
+              <ReCell
+                key={index}
+                fill={CHART_COLORS[index % CHART_COLORS.length]}
+              />
+            );
+          })}
+        </RePie>
+        <ReLegend align="right" layout="vertical" verticalAlign="middle" />
+      </RePieChart>
+    </ResponsiveContainer>
   );
 };
