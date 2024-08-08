a508bc30363fd0c0d1e9ce74c91af3a834f200ee
kimizuy <kimizuyamasaki@gmail.com>
2024-08-07 20:21:56 -0700

    remove sample code

--- app/src/pages/home/index.tsx
+++ app/src/pages/home/index.tsx
@@ -1,85 +1,10 @@
-import { Button, Field, Input } from "@fluentui/react-components";
-import { useEffect, useState } from "react";
-import { type FormProps } from "react-router-dom";
 import { Map } from "../../components/map";
 import { _dummyBuildingData } from "../../components/map/_dummy-data";
 
 export function Home(): JSX.Element {
-  const [names, setNames] = useState<string[] | null>(null);
-
-  const updateNames = async (): Promise<void> => {
-    const result = await window.ipcRenderer.invoke("getNames");
-    setNames(result);
-  };
-
-  const sayHelloFromPython = async (): Promise<void> => {
-    const result = await window.ipcRenderer.invoke("helloFromPython", "world");
-    console.info(result);
-  };
-
-  useEffect(() => {
-    const asyncEffect = async (): Promise<void> => {
-      await sayHelloFromPython();
-      await updateNames();
-    };
-    asyncEffect().catch(console.error);
-  }, []);
-
-  const handleSubmit: FormProps["onSubmit"] = (e) => {
-    e.preventDefault();
-    const asyncSubmit = async (): Promise<void> => {
-      const data = Object.fromEntries(new FormData(e.currentTarget));
-      await window.ipcRenderer.invoke("saveName", data.name.toString());
-      await updateNames();
-    };
-    asyncSubmit().catch(console.error);
-  };
-
-  const handleSubmitPython: FormProps["onSubmit"] = (e) => {
-    e.preventDefault();
-    const asyncSubmit = async (): Promise<void> => {
-      const data = Object.fromEntries(new FormData(e.currentTarget));
-      await window.ipcRenderer.invoke(
-        "saveNameFromPython",
-        data.name.toString(),
-      );
-      await updateNames();
-    };
-    asyncSubmit().catch(console.error);
-  };
-
   return (
     <div>
-      <h1>Hello, world!</h1>
-      <div>
-        <a href="#about">Go to about page</a>
-      </div>
-      <div>
-        <Map data={_dummyBuildingData} />
-      </div>
-      <form onSubmit={handleSubmit}>
-        <Field label="Save name">
-          <Input name="name" />
-        </Field>
-        <Button appearance="primary" type="submit">
-          Submit
-        </Button>
-        <Button appearance="secondary" type="reset">
-          Reset
-        </Button>
-      </form>
-      <form onSubmit={handleSubmitPython}>
-        <Field label="Save name from python">
-          <Input name="name" />
-        </Field>
-        <Button appearance="primary" type="submit">
-          Submit
-        </Button>
-        <Button appearance="secondary" type="reset">
-          Reset
-        </Button>
-      </form>
-      <div>{names?.map((message, i) => <p key={i}>{message}</p>)}</div>
+      <Map data={_dummyBuildingData} />
     </div>
   );
 }
