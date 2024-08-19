a7df384e5d3aa988bc4e8e9bf44b1066ceddb15a
WataruNishimura <wataru.n@chot-inc.com>
2024-08-19 15:26:19 +0900

    feat: resultView取得時にleftJoinで同時にdataSetResultを取得するように

--- app/src/hooks/use-fetch-result-views.ts
+++ app/src/hooks/use-fetch-result-views.ts
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react";
-import { type result_views } from "../schema";
+import { type data_set_results, type result_views } from "../schema";
 
 type ResultViews = typeof result_views.$inferSelect;
+type DataSetResults = typeof data_set_results.$inferSelect;
+type Result = {
+  result_views: ResultViews;
+  data_set_results: DataSetResults | null;
+};
 
 export const useFetchResultViews = ({
   sheetId,
 }: {
   sheetId: number | undefined;
-}): { data: ResultViews[]; refetch: (sheetId: number) => Promise<void> } => {
-  const [resultViews, setResultViews] = useState<ResultViews[]>([]);
+}): { data: Result[]; refetch: (sheetId: number) => Promise<void> } => {
+  const [resultViews, setResultViews] = useState<Result[]>([]);
 
   const fetchResultViews = async (id: number): Promise<void> => {
     const result = await window.ipcRenderer.invoke("selectResultViews", {
