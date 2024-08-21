4c75061af4bcb761702f3f37f1c35621f754170b
WataruNishimura <wataru.n@chot-inc.com>
2024-08-21 20:58:00 +0900

    fix: カラースキーマを変更

--- app/src/config/chart-colors.ts
+++ app/src/config/chart-colors.ts
@@ -1,17 +1,16 @@
-export const CHART_COLORS = [
-    "#FF6633",
-    "#FFB399",
-    "#FF33FF",
-    "#FFFF99",
-    "#00B3E6",
-    "#E6B333",
-    "#3366E6",
-    "#999966",
-    "#99FF99",
-    "#B34D4D",
-];
-
-export const GRAPH_COLORS = {
+export const CHART_COLORS = {
     primary: "#53389E",
     teritiary: "#B692F6",
+    repeated: [
+        "#FF6633",
+        "#FFB399",
+        "#FF33FF",
+        "#FFFF99",
+        "#00B3E6",
+        "#E6B333",
+        "#3366E6",
+        "#999966",
+        "#99FF99",
+        "#B34D4D",
+    ]
 }
