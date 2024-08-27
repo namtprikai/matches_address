a02e9a4f0d503a5e7afb6e20979ae817d31aea87
ksyunnnn <is.syunsukekobashi@gmail.com>
2024-08-27 12:13:44 +0900

    データセットリサルトの見た目を調整

---
+++ app/src/components/list-data-set-results.tsx
@@ -0,0 +1,61 @@
+import {
+  Body1,
+  Caption1,
+  makeStyles,
+  tokens,
+} from "@fluentui/react-components";
+import { type data_set_results } from "../schema";
+import { formatDate } from "../utils/format-date";
+import { Button } from "./ui/button";
+
+type DataSetResult = typeof data_set_results.$inferSelect;
+
+type Props = {
+  dataSetResults: DataSetResult[];
+  onClickItem?: (dataSetResult: DataSetResult) => void;
+};
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  button: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+    gap: tokens.spacingVerticalXS,
+  },
+  term: {
+    display: "flex",
+    /** @fixme TokensからPrimaryカラーにアクセスする方法がわからない */
+    backgroundColor: "#E9EAF6",
+    color: "#6264A7",
+    padding: tokens.spacingHorizontalXS,
+    borderRadius: tokens.borderRadiusMedium,
+  },
+});
+
+export const ListDataSetResults = ({
+  dataSetResults,
+  onClickItem,
+}: Props): JSX.Element => {
+  const styles = useStyles();
+  return (
+    <div className={styles.root}>
+      {dataSetResults.map((item) => (
+        <Button
+          key={item.id}
+          appearance="subtle"
+          className={styles.button}
+          onClick={() => onClickItem && onClickItem(item)}
+          shape="square"
+        >
+          <Body1>{item.title}</Body1>
+          <Caption1 className={styles.term}>
+            {formatDate(item.created_at, "YYYY/MM/DD")}
+          </Caption1>
+        </Button>
+      ))}
+    </div>
+  );
+};
