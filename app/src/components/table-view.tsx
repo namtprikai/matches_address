be09c54de0911b22f907e2f3ed880568f4b8e1a0
WataruNishimura <wataru.n@chot-inc.com>
2024-08-29 10:39:32 +0900

    feat:  表形式表示用のスタイルを追加

--- app/src/components/table-view.tsx
+++ app/src/components/table-view.tsx
@@ -1,16 +1,36 @@
 import {
+  makeStyles,
   Table,
   TableBody,
   TableCell,
   TableHeader,
   TableHeaderCell,
   TableRow,
+  tokens,
 } from "@fluentui/react-components";
 import {
   type data_set_detail_areas,
   type data_set_detail_buildings,
 } from "../schema";
 import { useFetchFilterDataSetForTable } from "../hooks/use-fetch-filtered-data-set-for-table";
+
+const useStyles = makeStyles({
+  tableHeader: {
+    backgroundColor: tokens.colorNeutralBackground3,
+  },
+  tableHeaderRow: {
+    border: "none",
+  },
+  tableHeaderCell: {
+    fontWeight: tokens.fontWeightSemibold,
+    padding: `${tokens.spacingVerticalS} ${tokens.spacingHorizontalM}`,
+  },
+  table: {
+    tableLayout: "auto",
+    width: "auto",
+    whiteSpace: "nowrap",
+  },
+});
 
 type TableViewProps = {
   resultId: number;
@@ -38,30 +58,42 @@ export const TableView = ({
     columns,
   });
 
+  const styles = useStyles();
+
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          {tableProps.columns.map((column, index) => {
-            return (
-              <TableHeaderCell key={index}>{column.label}</TableHeaderCell>
-            );
-          })}
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {tableProps.data
-          .map((row, index) => {
-            return (
-              <TableRow key={index}>
-                {tableProps.columns.map((column, index) => {
-                  return <TableCell key={index}>{row[column.key]}</TableCell>;
-                })}
-              </TableRow>
-            );
-          })
-          .flat(-1)}
-      </TableBody>
-    </Table>
+    <div>
+      {/* FIXME: overflowが機能しない */}
+      <Table className={styles.table}>
+        <TableHeader className={styles.tableHeader}>
+          <TableRow className={styles.tableHeaderRow}>
+            {tableProps.columns.map((column, index) => {
+              return (
+                <TableHeaderCell key={index} className={styles.tableHeaderCell}>
+                  {column.label}
+                </TableHeaderCell>
+              );
+            })}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {tableProps.data
+            .map((row, index) => {
+              return (
+                <TableRow key={index}>
+                  {tableProps.columns.map((column, index) => {
+                    return (
+                      <TableCell key={index}>
+                        {row[column.key]}
+                        {column.unit ?? ""}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })
+            .flat(-1)}
+        </TableBody>
+      </Table>
+    </div>
   );
 };
