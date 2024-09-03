c0b33db9366db2fa7b16176d172c5b7826b40de7
WataruNishimura <wataru.n@chot-inc.com>
2024-09-03 19:07:54 +0900

    feat: SQLでのサブクエリを利用したグルーピングの一部を実装

--- app/src/utils/subquery-grouping.test.ts
+++ app/src/utils/subquery-grouping.test.ts
@@ -1,10 +1,13 @@
 import Database from "better-sqlite3";
 import { drizzle, type BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
-import { afterAll, beforeAll, describe, test } from "vitest";
-import { data_set_detail_buildings } from "../schema";
+import { afterAll, beforeAll, describe, expect, test } from "vitest";
+import { a } from "vitest/dist/chunks/suite.CcK46U-P.js";
+import { data_set_detail_areas, data_set_detail_buildings } from "../schema";
+import { subqueryGrouping } from "./subquery-grouping";
 
 let dz: BetterSQLite3Database;
+const count = 5;
 
 beforeAll(async () => {
     const dbPath = ":memory:";
@@ -12,9 +15,33 @@ beforeAll(async () => {
     const db = new Database(dbPath, { verbose: console.log })
     dz = drizzle(db);
     await migrate(dz, { migrationsFolder: "drizzle" });
+
+    for (let i = 0; i < count; i++) {
+        dz.insert(data_set_detail_buildings).values({
+            id: i,
+            data_set_result_id: 1,
+            reference_date: `2024-0${i + 1}-01`,
+            depth: (i + 1) * 10,
+            measuredheight: (i + 1) * 10,
+        }).then().catch(console.error);
+    }
+
 })
+
 
 describe("サブクエリを利用したグルーピングのテスト", () => {
     test("サブクエリの実行", () => {
+        const res = subqueryGrouping(dz, data_set_detail_buildings, "depth_group", "depth", [
+            { operation: "eq", value: 10, label: "eq10" },
+            { operation: "noteq", value: 20, label: "noteq20" },
+        ]);
+
+
+        expect(res).toStrictEqual(
+            [
+                { depth_group: "eq10", depth: 10 },
+                { depth_group: "noteq20", depth: 40 },
+            ]
+        )
     });
 });
--- app/src/utils/subquery-grouping.ts
+++ app/src/utils/subquery-grouping.ts
@@ -1,5 +1,10 @@
-import { type SQL, sql } from "drizzle-orm";
-import { SQLiteSyncDialect } from "drizzle-orm/sqlite-core";
+import Database from "better-sqlite3";
+import { type Query, type SQL, sql, type Subquery } from "drizzle-orm";
+import { drizzle, type BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
+import { migrate } from "drizzle-orm/better-sqlite3/migrator";
+import { type SubqueryWithSelection } from "drizzle-orm/pg-core";
+import { type SQLiteSelect, type SQLiteSelectBase, SQLiteSyncDialect, type SQLiteTableWithColumns } from "drizzle-orm/sqlite-core";
+import { data_set_detail_buildings } from "../schema";
 
 type Condition = {
     operation: "eq" | "noteq",
@@ -12,7 +17,7 @@ const conditionsToCaseQuery = (key: string, conditions: Condition[
 
     const conditionSQL: SQL[] = conditions.map((condition) => { return sql.raw(`when ${key} ${condition.operation === "eq" ? "=" : "<>"} ${condition.value} then '${condition.label}'`) })
 
-    return sql`case ${sql.join(conditionSQL, sql.raw(" "))} end`;
+    return sql`*, case ${sql.join(conditionSQL, sql.raw(" "))} end`;
 }
 
 if (import.meta.vitest) {
@@ -28,6 +33,64 @@ if (import.meta.vitest) {
                 ]
             ))
 
-        ).toStrictEqual(sqliteDialect.sqlToQuery(sql`case when age = 1 then 'a' when age <> 2 then 'b' end`))
+        ).toStrictEqual(sqliteDialect.sqlToQuery(sql`*, case when age = 1 then 'a' when age <> 2 then 'b' end`))
     })
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type -- ignore
+const subQueryFromConditions = <T extends SQLiteTableWithColumns<any>>(drizzle: BetterSQLite3Database, db: T, groupLabel: string, key: string, conditions: Condition[]) => {
+    const caseQuery = conditionsToCaseQuery(key, conditions);
+
+    const subQuery = drizzle.select({
+        [groupLabel]: sql.join([caseQuery, sql.raw(`as ${groupLabel}`)], sql.raw(" ")),
+    }).from(db)
+
+    return subQuery;
+}
+
+if (import.meta.vitest) {
+    const { it, expect } = import.meta.vitest
+
+    it("条件からサブクエリを作成する", async () => {
+        const dbPath = ":memory:";
+        // eslint-disable-next-line no-console -- for debug
+        const db = new Database(dbPath, { verbose: console.log })
+        const dz = drizzle(db);
+        await migrate(dz, { migrationsFolder: "drizzle" });
+
+        const subquery =
+            subQueryFromConditions(
+                dz,
+                data_set_detail_buildings,
+                "group",
+                "age",
+                [
+                    { operation: "eq", value: 1, label: "a" },
+                    { operation: "noteq", value: 2, label: "b" },
+                ]
+            )
+
+        expect(
+            subquery.toSQL()
+        ).toStrictEqual(
+            {
+                sql: "select *, case when age = 1 then 'a' when age <> 2 then 'b' end as group from \"data_set_detail_buildings\"",
+                params: [],
+            }
+        )
+    })
+}
+
+export const subqueryGrouping = <T extends SQLiteTableWithColumns<any>>(drizzle: BetterSQLite3Database, db: T, groupLabel: string, key: string, conditions: Condition[]) => {
+    const subQuery = subQueryFromConditions(drizzle, db, groupLabel, key, conditions);
+
+    const query = drizzle.select({
+        [groupLabel]: sql.raw(`${groupLabel}`),
+        [key]: sql.raw(`avg(${key}) as ${key}`),
+    }).from(subQuery.as("groups")).groupBy(sql.raw(`${groupLabel}`)).having(sql.raw(`${groupLabel} <> ''`));
+
+    console.log(query.toSQL())
+
+
+    return query.all();
 }
