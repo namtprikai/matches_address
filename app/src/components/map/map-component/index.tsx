14a509dbc145243089a4a56a3c72980ed1c5a955
kimizuy <kimizuyamasaki@gmail.com>
2024-09-09 21:05:10 -0700

    fix visibility layer by vacancy levels

--- app/src/components/map/map-component/index.tsx
+++ app/src/components/map/map-component/index.tsx
@@ -158,6 +158,15 @@ export function MapComponent({
     function applyFiltersEffect() {
       if (!mapInstance || !layerIds?.length) return;
 
+      const allFalse =
+        !vacancyLevels.low && !vacancyLevels.medium && !vacancyLevels.high;
+      if (allFalse) {
+        for (const layerId of layerIds) {
+          mapInstance.setLayoutProperty(layerId, "visibility", "none");
+        }
+        return;
+      }
+
       for (const layerId of layerIds) {
         const filters = [];
         if (vacancyLevels.low) {
@@ -174,10 +183,8 @@ export function MapComponent({
           filters.push([">=", ["get", "predicted_probability"], 0.8]);
         }
 
-        const mapLibreFilter: FilterSpecification | undefined =
-          filters.length > 0
-            ? (["any", ...filters] as FilterSpecification)
-            : undefined;
+        const mapLibreFilter = ["any", ...filters] as FilterSpecification;
+
         mapInstance.setFilter(layerId, mapLibreFilter);
       }
     },
