80c544f037e20fed1f3b8abb2a8c40bb0d23f0a8
WataruNishimura <wataru.n@chot-inc.com>
2024-09-17 18:19:59 +0900

    feat: D902 / D903の地域区分のカラムを追加もしくは名称を変更

--- app/src/ipc-main-listeners/create-data-set-results.ts
+++ app/src/ipc-main-listeners/create-data-set-results.ts
@@ -51,6 +51,7 @@ export const createDataSetResults = (async (
         data_set_result_id: res[0].id,
         household_code: `1000000${i}`,
         normalized_address: `東京都港区六本木${i}丁目`,
+        area_group: `エリアグループ${i}`,
         reference_date: `202${i}-01-01`,
         household_size: number_of_people_in_household,
         members_under_15: number_of_people_under_15_years_old,
