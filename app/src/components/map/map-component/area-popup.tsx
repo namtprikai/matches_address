8a94da71a1efc668004bb92e3f172c251e5980b5
kimizuy <kimizuyamasaki@gmail.com>
2024-09-17 20:34:07 -0700

    fix unit

--- app/src/components/map/map-component/area-popup.tsx
+++ app/src/components/map/map-component/area-popup.tsx
@@ -55,13 +55,13 @@ export const AreaPopup = forwardRef<HTMLDivElement, Props>(
             <div className={styles.item}>
               <span className={styles.itemLabel}>若年層率</span>
               <span className={styles.itemValue}>
-                {properties.young_population_ratio}人
+                {properties.young_population_ratio}%
               </span>
             </div>
             <div className={styles.item}>
               <span className={styles.itemLabel}>高年者率</span>
               <span className={styles.itemValue}>
-                {properties.elderly_population_ratio}
+                {properties.elderly_population_ratio}%
               </span>
             </div>
           </div>
@@ -77,7 +77,7 @@ export const AreaPopup = forwardRef<HTMLDivElement, Props>(
             <div className={styles.item}>
               <span className={styles.itemLabel}>空き家件数</span>
               <span className={styles.itemValue}>
-                {properties.vacant_house_count}
+                {properties.vacant_house_count}件
               </span>
             </div>
           </div>
