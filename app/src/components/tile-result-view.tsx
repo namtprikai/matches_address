df65354b2d4ea6e4d854e85f24f80278919c2865
WataruNishimura <wataru.n@chot-inc.com>
2024-09-18 09:50:46 +0900

    feat: 削除完了時にリロードできるよう変更

--- app/src/components/tile-result-view.tsx
+++ app/src/components/tile-result-view.tsx
@@ -12,6 +12,7 @@ import {
   tokens,
 } from "@fluentui/react-components";
 import { useAtom } from "jotai";
+import { useNavigate } from "react-router-dom";
 import { type data_set_results, type SelectDataSetResult } from "../schema";
 import { THEME_COLORS } from "../config/theme-colors";
 import { type SelectResultViewResponse } from "../ipc-main-listeners/select-result-view";
@@ -55,6 +56,7 @@ export const TileResultView = ({
   ...cardProps
 }: Props): JSX.Element => {
   const styles = useStyles();
+  const navigate = useNavigate();
 
   const [, refreshResultViews] = useAtom(resultViewsAtom);
 
@@ -67,6 +69,7 @@ export const TileResultView = ({
   const handleDelete = async (): Promise<void> => {
     await deleteResultView();
     refreshResultViews();
+    navigate(0);
   };
 
   if (
