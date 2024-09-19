2b2d6dc4ab8f421e83f0ce1e7249da849afc370a
WataruNishimura <wataru.n@chot-inc.com>
2024-09-19 21:49:22 +0900

    feat: 集計単位に合わせて地域区分を取得するAPIを作成

---
+++ app/src/ipc-main-listeners/fetch-area-groups.ts
@@ -0,0 +1,44 @@
+import { eq } from "drizzle-orm";
+import {
+  data_set_detail_areas,
+  data_set_detail_buildings,
+  type SelectDataSetResult,
+} from "../schema";
+import { db } from "../utils/db";
+import { type IpcMainListener } from ".";
+
+export type FetchAreaGroupsArg = {
+  dataSetResultId: SelectDataSetResult["id"];
+  unit: "building" | "area";
+};
+
+export const fetchAreaGroups = (async (
+  _: unknown,
+  props: FetchAreaGroupsArg,
+): Promise<string[]> => {
+  const result =
+    props.unit === "building"
+      ? await db
+          .selectDistinct({
+            area_group: data_set_detail_buildings.area_group,
+          })
+          .from(data_set_detail_buildings)
+          .where(
+            eq(
+              data_set_detail_buildings.data_set_result_id,
+              props.dataSetResultId,
+            ),
+          )
+      : await db
+          .selectDistinct({
+            area_group: data_set_detail_buildings.area_group,
+          })
+          .from(data_set_detail_areas)
+          .where(
+            eq(data_set_detail_areas.data_set_result_id, props.dataSetResultId),
+          );
+
+  const areas = result.map((r) => r.area_group);
+
+  return areas.filter((g) => g !== null);
+}) satisfies IpcMainListener;
