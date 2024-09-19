59662bc45a5f4dd4399712939b99b6fdd4820d3e
kimizuy <kimizuyamasaki@gmail.com>
2024-09-18 17:01:17 -0700

    return date & refactor

--- app/src/ipc-main-listeners/fetch-reference-dates.ts
+++ app/src/ipc-main-listeners/fetch-reference-dates.ts
@@ -1,22 +1,27 @@
 import { eq } from "drizzle-orm";
-import { data_set_detail_buildings, type SelectDataSetResult } from "../schema";
+import {
+  data_set_detail_buildings,
+  type SelectDataSetDetailBuilding,
+  type SelectDataSetResult,
+} from "../schema";
 import { db } from "../utils/db";
 import { type IpcMainListener } from ".";
+
+export type ReferenceDate = SelectDataSetDetailBuilding["reference_date"];
 
 /**
  * "2020-01-01" などの日付を配列で取得する
  */
-export const fetchReferenceDates = ((
+export const fetchReferenceDates = (async (
   _: unknown,
   { dataSetResultId }: { dataSetResultId: SelectDataSetResult["id"] },
-): string[] => {
-  const result = db
+): Promise<ReferenceDate[]> => {
+  const result = await db
     .selectDistinct({
       reference_date: data_set_detail_buildings.reference_date,
     })
     .from(data_set_detail_buildings)
-    .where(eq(data_set_detail_buildings.data_set_result_id, dataSetResultId))
-    .all();
+    .where(eq(data_set_detail_buildings.data_set_result_id, dataSetResultId));
 
   return result.map((r) => r.reference_date);
 }) satisfies IpcMainListener;
