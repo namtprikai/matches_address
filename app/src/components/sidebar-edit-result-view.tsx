05c282fbd11b47db4187977ba9d6ba7fe97b46c4
ksyunnnn <is.syunsukekobashi@gmail.com>
2024-09-23 18:37:34 +0900

    ビュー追加時にlayoutIndexも追加

--- app/src/components/sidebar-edit-result-view.tsx
+++ app/src/components/sidebar-edit-result-view.tsx
@@ -57,6 +57,7 @@ export const SidebarEditResultView = (): JSX.Element => {
     await window.ipcRenderer.invoke("insertResultViews", {
       data_set_result_id: dataSetResultId,
       sheet_id: selectedResultSheetId,
+      layoutIndex: resultViews.length,
       parameters: [],
     });
     // TODO: できればリロードせずに更新したい
