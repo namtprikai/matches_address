33ba6eccae5fc10953e05d5384b00bf4b6e9e191
WataruNishimura <wataru.n@chot-inc.com>
2024-09-30 16:53:52 +0900

    build: react devtools の導入設定

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,11 +1,13 @@
-import { app, BrowserWindow, ipcMain } from "electron";
-import path from "path";
+import { app, BrowserWindow, ipcMain, session } from "electron";
+import { join } from "path";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
 import { db } from "./utils/db";
 import { ipcMainListeners } from "./ipc-main-listeners";
+import os from "os";
+import { readdirSync } from "fs";
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
@@ -15,13 +17,15 @@ if (require("electron-squirrel-startup")) {
 const hono = new Hono();
 const port = 3000;
 
+const development = process.env.ELECTRON_ENV === "development";
+
 const createWindow = (): void => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 1200,
     height: 900,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
+      preload: join(__dirname, "preload.js"),
     },
   });
 
@@ -40,19 +44,16 @@ const createWindow = (): void => {
   }
 };
 
-void app.whenReady().then(() => {
+void app.whenReady().then(async () => {
   const migrationsFolder =
     process.env.NODE_ENV === "development"
       ? "drizzle"
-      : path.join(process.resourcesPath, "drizzle");
+      : join(process.resourcesPath, "drizzle");
   migrate(db, { migrationsFolder });
 
   if (!MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     // Set up Hono server for production
-    const distPath = path.join(
-      __dirname,
-      `../renderer/${MAIN_WINDOW_VITE_NAME}`,
-    );
+    const distPath = join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}`);
 
     // Serve static files
     hono.use("/*", serveStatic({ root: distPath }));
@@ -74,12 +75,29 @@ void app.whenReady().then(() => {
     ipcMain.handle(channel, listener);
   });
 
-  app.on("activate", () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
+  // React DevTool Path
+  const reactDevToolExtensionPath = getReactDevToolsPath();
+
+  // if React DevTool is not installed
+  if (!reactDevToolExtensionPath || !development) {
+    app.on("activate", () => {
+      // On OS X it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+      }
+    });
+    return;
+  }
+
+  session.defaultSession.loadExtension(reactDevToolExtensionPath).then(() => {
+    app.on("activate", () => {
+      // On OS X it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+      }
+    });
   });
 });
 
@@ -94,3 +112,25 @@ app.on("window-all-closed", () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+const getReactDevToolsPath = () => {
+  const devtoolsId = "fmkadmapgofadopljbjfkapdkoienihi";
+  const platform = os.platform();
+
+  switch (platform) {
+    case "win32":
+      const winDevToolsInstallPath = `${process.env.LOCALAPPDATA}\\Google\\Chrome\\User Data\\Default\\Extensions\\${devtoolsId}\\`;
+      const dirs = readdirSync(winDevToolsInstallPath);
+      return join(winDevToolsInstallPath, dirs[0]);
+    case "darwin":
+      const macDevToolsInstallPath = `${os.homedir()}/Library/Application Support/Google/Chrome/Default/Extensions/${devtoolsId}/`;
+      const macDirs = readdirSync(macDevToolsInstallPath);
+      return join(macDevToolsInstallPath, macDirs[0]);
+    case "linux":
+      const linuxDevToolsInstallPath = `${os.homedir()}/.config/google-chrome/Default/Extensions/${devtoolsId}/`;
+      const linuxDirs = readdirSync(linuxDevToolsInstallPath);
+      return join(linuxDevToolsInstallPath, linuxDirs[0]);
+    default:
+      return null;
+  }
+};
