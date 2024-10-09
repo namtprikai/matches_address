130f9b064b1de06f0a8da9725cd672e205a689c7
WataruNishimura <wataru.n@chot-inc.com>
2024-10-09 20:44:42 +0900

    fix: importエラーの問題を修正

--- app/src/components/bi/form-edit-result-view.tsx
+++ app/src/components/bi/form-edit-result-view.tsx
@@ -8,9 +8,9 @@ import { type SelectResultView } from "../../schema";
 import { useFetchDataSetResultItem } from "../../hooks/use-fetch-data-set-result-item";
 import { useFetchResultView } from "../../hooks/use-fetch-result-view";
 import { useFetchResultViews } from "../../hooks/use-fetch-result-views";
+import { Button } from "../ui/button";
 import { EditResultViewFields } from "./edit-result-view-fields";
 import { EditResultViewFilterFields } from "./edit-result-view-filter-fields";
-import { Button } from "../ui/button";
 
 const useStyles = makeStyles({
   form: {
