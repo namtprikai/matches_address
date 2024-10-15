1a7d6f3d4c0c2f24e6725dcf47662d25a8f38e35
ksyunnnn <is.syunsukekobashi@gmail.com>
2024-10-15 12:44:59 +0900

    デバッグコードがnoteを追加できるように修正

--- app/src/ipc-main-listeners/_debug-insert-model-files.ts
+++ app/src/ipc-main-listeners/_debug-insert-model-files.ts
@@ -4,7 +4,11 @@ import { type IpcMainListener } from ".";
 
 export const _debugInsertModelFiles = (async (): Promise<void> => {
   db.insert(model_files)
-    .values({ file_name: "test", file_path: "path/to/test.csv" })
+    .values({
+      file_name: "test",
+      file_path: "path/to/test.csv",
+      note: "このモデルはChatGPT、OpenAIによって開発されたGPT-4アーキテクチャに基づく大規模な言語モデルです。",
+    })
     .returning({ insertedId: model_files.id })
     .get();
 }) satisfies IpcMainListener;
