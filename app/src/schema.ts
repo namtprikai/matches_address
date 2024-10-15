b5036a7f00beca8e593d93d3412a117ac0e1120c
ksyunnnn <is.syunsukekobashi@gmail.com>
2024-10-15 12:54:32 +0900

    不要なraw_datasetsのjob_results_idカラムを削除

--- app/src/schema.ts
+++ app/src/schema.ts
@@ -611,7 +611,6 @@ export const raw_data_sets = sqliteTable("raw_data_sets", {
   file_name: text("file_name").notNull(),
   // job_resultsの内部パス / NOT NULL
   file_path: text("file_path").notNull(),
-  job_results_id: integer("job_results_id").notNull(),
 
   created_at: text("created_at")
     .default(sql`(CURRENT_TIMESTAMP)`)
