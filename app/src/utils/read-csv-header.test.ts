4903e660d8419277213f44833aa0124b2087eeb0
WataruNishimura <wataru.n@chot-inc.com>
2024-10-21 13:21:57 +0900

    remove: PRに含める必要のないファイルを削除

--- app/src/utils/read-csv-header.test.ts
+++
@@ -1,9 +0,0 @@
-import { join } from "path";
-import { expect, test } from "vitest";
-import { readCSVHeader } from "./read-csv-header";
-
-test("CSVヘッダーの一行目を読み取る", async () => {
-  const filePath = "./test-sample.csv";
-  const header = await readCSVHeader(join(__dirname, filePath));
-  expect(header).toEqual(["Name", "Age", "Email", "Country", "Occupation"]);
-});
