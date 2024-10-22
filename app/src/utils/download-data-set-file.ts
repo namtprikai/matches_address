e83c5d4327e1db0d27d99efd6cf1739467d56a34
kimizuy <kimizuyamasaki@gmail.com>
2024-10-21 20:11:22 -0700

    add .csv extension when no extension

--- app/src/utils/download-data-set-file.ts
+++ app/src/utils/download-data-set-file.ts
@@ -1,8 +1,10 @@
 export function downloadDataSetFile(buffer: Buffer, fileName: string): void {
+  const hasExtension = fileName.includes(".");
+  const name = hasExtension ? fileName : `${fileName}.csv`; // ファイル名に拡張子が含まれていない場合は、とりあえず.csvを付与する
   const url = URL.createObjectURL(new Blob([buffer]));
   const link = document.createElement("a");
   link.href = url;
-  link.download = fileName;
+  link.download = name;
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
