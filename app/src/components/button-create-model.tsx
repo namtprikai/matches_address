d011397962e889ab92bdc65c9ad3b710f07190a4
ksyunnnn <is.syunsukekobashi@gmail.com>
2024-10-23 17:55:41 +0900

    MenuItemをクリックする方法を修正

--- app/src/components/button-create-model.tsx
+++ app/src/components/button-create-model.tsx
@@ -1,6 +1,5 @@
 import { AddFilled } from "@fluentui/react-icons";
 import {
-  makeStyles,
   Menu,
   MenuItem,
   MenuList,
@@ -8,65 +7,47 @@ import {
   MenuTrigger,
   tokens,
 } from "@fluentui/react-components";
-import { Link } from "react-router-dom";
-import { useDialogState } from "../hooks/use-dialog-state";
+import { useNavigate } from "react-router-dom";
 import { Button } from "./ui/button";
-import { DialogImportDataset } from "./dialog-import-dataset";
-
-const useStyles = makeStyles({
-  navigationToNormalization: {
-    textDecoration: "none",
-    color: "inherit",
-    display: "block",
-  },
-});
 
 export const ButtonCreateModel = (): JSX.Element => {
-  const styles = useStyles();
-
-  const dialogState = useDialogState(false);
+  const navigator = useNavigate();
 
   return (
-    <>
-      <Menu>
-        <MenuTrigger disableButtonEnhancement>
-          <Button
-            icon={
-              <AddFilled
-                color={tokens.colorNeutralForeground1}
-                fontSize={tokens.fontSizeBase400}
-                strokeWidth={2}
-              />
-            }
-            size="small"
+    <Menu>
+      <MenuTrigger disableButtonEnhancement>
+        <Button
+          icon={
+            <AddFilled
+              color={tokens.colorNeutralForeground1}
+              fontSize={tokens.fontSizeBase400}
+              strokeWidth={2}
+            />
+          }
+          size="small"
+        >
+          新規モデル作成
+        </Button>
+      </MenuTrigger>
+
+      <MenuPopover>
+        <MenuList>
+          <MenuItem
+            onClick={() => {
+              navigator("/normalization");
+            }}
           >
-            新規モデル作成
-          </Button>
-        </MenuTrigger>
-
-        <MenuPopover>
-          <MenuList>
-            <MenuItem>
-              <Link
-                className={styles.navigationToNormalization}
-                to={"/normalization"}
-              >
-                正規化処理から始める
-              </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link
-                className={styles.navigationToNormalization}
-                to={"/model/create"}
-              >
-                正規化処理済データから始める
-              </Link>
-            </MenuItem>
-          </MenuList>
-        </MenuPopover>
-      </Menu>
-
-      <DialogImportDataset dialogState={dialogState} />
-    </>
+            正規化処理から始める
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              navigator("/model/create");
+            }}
+          >
+            正規化処理済データから始める
+          </MenuItem>
+        </MenuList>
+      </MenuPopover>
+    </Menu>
   );
 };
