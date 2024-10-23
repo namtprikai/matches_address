8a76e431bccd6151b0bfaed658952395e3876b93
ksyunnnn <is.syunsukekobashi@gmail.com>
2024-10-23 13:27:19 +0900

    updateリスナーから不要な返却値を削除

--- app/src/ipc-main-listeners/update-model-files.ts
+++ app/src/ipc-main-listeners/update-model-files.ts
@@ -1,9 +1,5 @@
 import { eq } from "drizzle-orm";
-import {
-  type InsertModelFile,
-  model_files,
-  type SelectModelFile,
-} from "../schema";
+import { type InsertModelFile, model_files } from "../schema";
 import { db } from "../utils/db";
 import { type IpcMainListener } from ".";
 
@@ -14,12 +10,9 @@ type Params = {
 export const updateModelFiles = (async (
   _: unknown,
   { modelFileId, value: { file_name, note } }: Params,
-): Promise<SelectModelFile[]> => {
-  const all = await db
+): Promise<void> => {
+  await db
     .update(model_files)
     .set({ file_name, note })
-    .where(eq(model_files.id, modelFileId))
-    .returning();
-
-  return all;
+    .where(eq(model_files.id, modelFileId));
 }) satisfies IpcMainListener;
