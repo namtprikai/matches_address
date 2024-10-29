0f89285d8c9229bdbbe1bf0a98f60ca836ea9b40
m-abe-dev <uuu.theman.jp@gmail.com>
2024-10-29 17:44:28 +0100

    fix: rader chart and css for graph

--- app/src/pages/job/detail/graph/index.tsx
+++ app/src/pages/job/detail/graph/index.tsx
@@ -15,7 +15,6 @@ import {
   Tooltip,
 } from "recharts";
 import { ArrowLeftRegular } from "@fluentui/react-icons";
-import { useState } from "react";
 import { DialogSaveWithName } from "../../../../components/dialog-save-with-name";
 
 const useStyles = makeStyles({
@@ -74,13 +73,6 @@ const useStyles = makeStyles({
     borderRadius: "100px",
     padding: `${tokens.spacingVerticalMNudge} ${tokens.spacingHorizontalL}`,
     height: "40px",
-  },
-  accurateContainer: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "left",
-    backgroundColor: "#fff",
-    padding: tokens.spacingVerticalXXL,
   },
   columnContainer: {
     display: "flex",
@@ -144,69 +136,6 @@ const useStyles = makeStyles({
     marginTop: tokens.spacingHorizontalS,
     height: "20px",
   },
-  gaugeContainer: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    marginBottom: tokens.spacingVerticalXL,
-    position: "relative",
-    backgroundColor: "#fff",
-    padding: tokens.spacingVerticalXL,
-    width: "241px",
-  },
-  gaugeWrapper: {
-    position: "relative",
-    width: "220px",
-    height: "110px",
-    overflow: "visible",
-  },
-  gaugeBackground: {
-    width: "100%",
-    height: "200%",
-    transform: "rotate(270deg)",
-  },
-  gaugeCircle: {
-    fill: "none",
-    stroke: "#E0E0E0",
-    strokeWidth: "30",
-    transform: "rotate(270deg)",
-    transformOrigin: "center",
-  },
-  gaugeArc: {
-    fill: "none",
-    stroke: "#C4314B",
-    strokeWidth: "30",
-    transform: "rotate(270deg)",
-    transformOrigin: "center",
-  },
-  gaugeText: {
-    position: "absolute",
-    top: "85%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    color: "#C4314B",
-    ...typographyStyles.title2,
-  },
-  gaugeLabelLeft: {
-    position: "absolute",
-    top: "85%",
-    left: "10%",
-    transform: "translateX(-10%)",
-    ...typographyStyles.caption1,
-  },
-  gaugeLabelRight: {
-    position: "absolute",
-    top: "85%",
-    right: "7%",
-    transform: "translateX(10%)",
-    ...typographyStyles.caption1,
-  },
-  detail: {
-    backgroundColor: "#FDF7F8",
-    padding: `${tokens.spacingVerticalS} ${tokens.spacingHorizontalM}`,
-    borderRadius: tokens.borderRadiusSmall,
-    width: "241px",
-  },
   graphWrapper: {
     display: "flex",
     gap: tokens.spacingHorizontalM,
@@ -214,37 +143,32 @@ const useStyles = makeStyles({
   radarChartContainer: {
     backgroundColor: "#fff",
     padding: tokens.spacingVerticalXXL,
-    width: "100%",
+    width: "448px",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
   },
+  titleWrapper: {
+    display: "flex",
+    alignItems: "flex-start",
+    width: "100%",
+  },
   radarChartTitle: typographyStyles.subtitle2,
+  detail: {
+    backgroundColor: "#FDF7F8",
+    padding: `${tokens.spacingVerticalS} ${tokens.spacingHorizontalM}`,
+    borderRadius: tokens.borderRadiusSmall,
+    width: "100%",
+  },
 });
 
 export function JobGraph(): JSX.Element {
   const styles = useStyles();
   const navigate = useNavigate();
-  // MEMO: ゲージメーターの表示を切り替えるためのフラグ 後でAPIから取得する
-  const [showGauge, setShowGauge] = useState(true);
-
-  const handleToggle = (): void => {
-    setShowGauge(!showGauge);
-  };
 
   const handleBack = (): void => {
     navigate(-1);
   };
-
-  // ゲージメーターの値（0〜100の間）
-  const gaugeValue = 15;
-
-  // ゲージの弧の長さを計算
-  const radius = 90; // 半径
-  const circumference = 2 * Math.PI * radius;
-
-  const halfCircumference = circumference / 2;
-  const halfOffset = halfCircumference - (gaugeValue / 100) * halfCircumference;
 
   const chartData = [
     {
@@ -297,57 +221,44 @@ export function JobGraph(): JSX.Element {
           <div className={styles.buttonWrapper}>
             <DialogSaveWithName />
             <Button className={styles.button}>ダウンロード</Button>
-            <Button onClick={handleToggle}>
-              {showGauge ? "正解率が高い場合" : "正解率が低い場合"}
-            </Button>
           </div>
         </div>
 
         <div className={styles.graphWrapper}>
-          {/* ゲージメーター */}
-          {showGauge && (
-            <div className={styles.accurateContainer}>
-              <div className={styles.columnTitle}>正解率</div>
-              <div className={styles.gaugeContainer}>
-                <div className={styles.gaugeWrapper}>
-                  <svg
-                    className={styles.gaugeBackground}
-                    viewBox="0 0 220 210"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    {/* 背景の半円弧 */}
-                    <circle
-                      className={styles.gaugeCircle}
-                      cx="110"
-                      cy="110"
-                      r={radius}
-                      strokeDasharray={`${halfCircumference} ${circumference}`}
-                      strokeDashoffset="0"
-                    />
-                    {/* 値を表す半円弧 */}
-                    <circle
-                      className={styles.gaugeArc}
-                      cx="110"
-                      cy="110"
-                      r={radius}
-                      strokeDasharray={`${halfCircumference} ${circumference}`}
-                      strokeDashoffset={halfOffset}
-                    />
-                  </svg>
-                  {/* メーターの中央に値を表示 */}
-                  <div className={styles.gaugeText}>{gaugeValue}%</div>
-                </div>
-                <div className={styles.gaugeLabelLeft}>0%</div>
-                <div className={styles.gaugeLabelRight}>100%</div>
-              </div>
-              <div className={styles.detail}>
-                学習データ量が少なすぎます。正答率を上げるためには、〇〇以上のデータに修正して再実行をしてください。
-              </div>
+          {/* RadarChart */}
+          <div className={styles.radarChartContainer}>
+            <div className={styles.titleWrapper}>
+              <span className={styles.radarChartTitle}>
+                処理結果のパラメーター
+              </span>
             </div>
-          )}
+            <RadarChart
+              cx={200}
+              cy={200}
+              data={radarData}
+              height={400}
+              outerRadius={150}
+              width={400}
+            >
+              <PolarGrid />
+              <PolarAngleAxis dataKey="subject" />
+              <PolarRadiusAxis angle={90} domain={[0, 100]} />
+              <Radar
+                dataKey="A"
+                fill="#8884d8"
+                fillOpacity={0.6}
+                name="指標"
+                stroke="#8884d8"
+              />
+              <Tooltip />
+            </RadarChart>
+            <div className={styles.detail}>
+              学習データ量が少なすぎます。正答率を上げるためには、〇〇以上のデータに修正して再実行をしてください。
+            </div>
+          </div>
           {/* 棒グラフ */}
           <div className={styles.columnContainer}>
-            <div className={styles.columnTitle}>重要度の高いカラム</div>
+            <span className={styles.columnTitle}>重要度の高いカラム</span>
             <div className={styles.chartContainer}>
               {/* Y軸のラベル */}
               <div className={styles.yAxisLabel}>
@@ -357,8 +268,6 @@ export function JobGraph(): JSX.Element {
                   </Text>
                 ))}
               </div>
-
-              {/* 棒グラフ */}
               <div style={{ flex: 1 }}>
                 <div className={styles.barContainer}>
                   {chartData.map((data, index) => (
@@ -395,32 +304,6 @@ export function JobGraph(): JSX.Element {
             </div>
           </div>
         </div>
-        {/* RadarChart */}
-        {!showGauge && (
-          <div className={styles.radarChartContainer}>
-            <div className={styles.radarChartTitle}>処理結果のパラメーター</div>
-            <RadarChart
-              cx={200}
-              cy={200}
-              data={radarData}
-              height={400}
-              outerRadius={150}
-              width={400}
-            >
-              <PolarGrid />
-              <PolarAngleAxis dataKey="subject" />
-              <PolarRadiusAxis angle={90} domain={[0, 100]} />
-              <Radar
-                dataKey="A"
-                fill="#8884d8"
-                fillOpacity={0.6}
-                name="指標"
-                stroke="#8884d8"
-              />
-              <Tooltip />
-            </RadarChart>
-          </div>
-        )}
       </div>
 
       <div className={styles.restartButtonWrapper}>
