f67d143dd4d61cf9fa04fcca4460f2335f863224
kimizuy <kimizuyamasaki@gmail.com>
2024-11-10 17:06:09 -0800

    update

--- app/src/components/dataset/result-dataset-metadata.ts
+++ app/src/components/dataset/result-dataset-metadata.ts
@@ -17,8 +17,14 @@ const buildingMetadata: Record<keyof SelectDataSetDetailBuilding, Item> = {
   data_set_result_id: {
     label: "data_set_result_id",
   },
-  household_code: {
-    label: "世帯番号",
+  residence_id: {
+    label: "residenceID",
+  },
+  // key_code: {
+  //   label: "KEY_CODE",
+  // },
+  reference_date: {
+    label: "基準日",
   },
   normalized_address: {
     label: "正規化住所",
@@ -26,15 +32,15 @@ const buildingMetadata: Record<keyof SelectDataSetDetailBuilding, Item> = {
   area_group: {
     label: "建物所属地域区分",
   },
-  reference_date: {
-    label: "基準日",
+  household_code: {
+    label: "世帯番号",
   },
   household_size: {
     label: "世帯人数",
     unit: "人",
   },
   members_under_15: {
-    label: "15歳未満の世帯人数",
+    label: "15歳未満人数",
     unit: "人",
   },
   percentage_under_15: {
@@ -42,7 +48,7 @@ const buildingMetadata: Record<keyof SelectDataSetDetailBuilding, Item> = {
     unit: "%",
   },
   members_15_to_64: {
-    label: "15歳以上64歳以下の世帯人数",
+    label: "15歳以上64歳以下人数",
     unit: "人",
   },
   percentage_15_to_64: {
@@ -50,7 +56,7 @@ const buildingMetadata: Record<keyof SelectDataSetDetailBuilding, Item> = {
     unit: "%",
   },
   members_over_65: {
-    label: "65歳以上の世帯人数",
+    label: "65歳以上人数",
     unit: "人",
   },
   percentage_over_65: {
@@ -64,6 +70,12 @@ const buildingMetadata: Record<keyof SelectDataSetDetailBuilding, Item> = {
   residence_duration: {
     label: "住定期間",
   },
+  // members_min_age: {
+  //   label: "世帯最少年齢",
+  // },
+  // member_max_age: {
+  //   label: "世帯最高年齢",
+  // },
   water_supply_number: {
     label: "水道番号",
   },
@@ -71,111 +83,65 @@ const buildingMetadata: Record<keyof SelectDataSetDetailBuilding, Item> = {
     label: "水道閉栓フラグ",
   },
   max_water_usage: {
-    label: "最大水道使用量",
+    label: "水道最大使用量",
     unit: "L",
   },
   avg_water_usage: {
-    label: "平均水道使用量",
-    unit: "L",
-  },
-  total_water_usage: {
-    label: "合計水道使用量",
+    label: "水道平均使用量",
     unit: "L",
   },
   min_water_usage: {
-    label: "最小水道使用量",
+    label: "水道最小使用量",
     unit: "L",
   },
+  total_water_usage: {
+    label: "水道合計使用量",
+    unit: "L",
+  },
+  // change_ratio_water_usage: {
+  //   label: "水道使用量変化率",
+  // },
   water_supply_source_info: {
-    label: "名寄せ元情報_水道",
+    label: "水道名寄せ元情報",
   },
   structure_name: {
     label: "建物構造名",
   },
   registration_date: {
-    label: "登記年月日",
+    label: "登録年月日",
   },
   registration_source_info: {
-    label: "名寄せ元情報_住基",
+    label: "登記名寄せ元情報",
   },
-  vacant_house_id: {
-    label: "ID_空き家結果_データクレンジング済",
+  geometry: {
+    label: "ジオメトリ",
   },
-  vacant_house_address: {
-    label: "住所_空き家結果_データクレンジング済",
-  },
-  vacant_house_longitude: {
-    label: "vacant_house_longitude",
-  },
-  vacant_house_latitude: {
-    label: "vacant_house_latitude",
-  },
-  vacant_house_source_info: {
-    label: "vacant_house_source_info",
-  },
-  geocoded_address: {
-    label: "geocoded_address",
-  },
-  geocoded_longitude: {
-    label: "geocoded_longitude",
-  },
-  geocoded_latitude: {
-    label: "geocoded_latitude",
-  },
-  geocoding_source_info: {
-    label: "geocoding_source_info",
-  },
-  has_water_supply: {
-    label: "has_water_supply",
-  },
-  has_juki_registry: {
-    label: "has_juki_registry",
-  },
-  has_touki_registry: {
-    label: "has_touki_registry",
-  },
-  has_juki_and_water: {
-    label: "has_juki_and_water",
-  },
-  has_vacant_result: {
-    label: "has_vacant_result",
-  },
-  has_juki_water_property: {
-    label: "has_juki_water_property",
-  },
-  has_geocoding: {
-    label: "has_geocoding",
-  },
-  has_juki_water_property_vacant: {
-    label: "has_juki_water_property_vacant",
-  },
+  // usage: {
+  //   label: "usage",
+  // },
   fid: {
     label: "fid",
-  },
-  gml_id: {
-    label: "GML ID",
   },
   class: {
     label: "class",
   },
-  geometry: {
-    label: "ジオメトリデータ",
+  gml_id: {
+    label: "gml_id",
   },
   measuredheight: {
     label: "浸水想定深",
-    unit: "m",
   },
   measuredheightUom: {
-    label: "measuredheight_uom",
+    label: "measuredheightUom",
+  },
+  thematic_src_desc: {
+    label: "thematic_src_desc",
   },
   src_scale: {
     label: "src_scale",
   },
   geometry_src_desc: {
     label: "geometry_src_desc",
-  },
-  thematic_src_desc: {
-    label: "thematic_src_desc",
   },
   lod1_height_type: {
     label: "lod1_height_type",
@@ -233,62 +199,125 @@ const buildingMetadata: Record<keyof SelectDataSetDetailBuilding, Item> = {
     label: "value_uom",
   },
   inland_flooding_risk_desc: {
-    label: "洪水浸水リスク属性_建物",
+    label: "inland_flooding_risk_desc",
   },
   inland_flooding_risk_rank: {
-    label: "洪水浸水リスク属性_ランク",
+    label: "inland_flooding_risk_rank",
   },
   inland_flooding_risk_depth: {
-    label: "洪水浸水リスク属性_浸水深",
+    label: "inland_flooding_risk_depth",
     unit: "m",
   },
   inland_flooding_risk_depth_uom: {
     label: "inland_flooding_risk_depth_uom",
   },
+  name: {
+    label: "name",
+  },
+  area_type: {
+    label: "areaType",
+  },
   river_flooding_risk_desc: {
-    label: "洪水氾濫リスク属性_建物",
+    label: "river_flooding_risk_desc",
   },
   river_flooding_risk_rank: {
-    label: "洪水氾濫リスク属性_ランク",
+    label: "river_flooding_risk_rank",
   },
   river_flooding_risk_depth: {
-    label: "洪水氾濫リスク属性_氾濫深長",
+    label: "river_flooding_risk_depth",
     unit: "m",
   },
   river_flooding_risk_depth_uom: {
     label: "river_flooding_risk_depth_uom",
   },
   landslide_risk_desc: {
-    label: "地滑りリスク属性_建物",
+    label: "landslide_risk_desc",
   },
   large_store_name: {
-    label: "large_store_name",
+    label: "大規模店舗名称",
   },
   appearance_src_desc: {
     label: "appearance_src_desc",
   },
+  // theme: {
+  //   label: "theme",
+  // },
+  // image_uri: {
+  //   label: "image_uri",
+  // },
+  // mimetype: {
+  //   label: "mimetype",
+  // },
+  // texture_coordinates: {
+  //   label: "texture_coordinates",
+  // },
   branch_id: {
     label: "branch_id",
-  },
-  residence_id: {
-    label: "residence_id",
   },
   is_test: {
     label: "is_test",
   },
-  name: {
-    label: "建物名",
-  },
-  area_type: {
-    label: "area_type",
-  },
   predicted_label: {
-    label: "空き家確率",
+    label: "predicted_label",
   },
   predicted_probability: {
-    label: "空き家確率",
+    label: "predicted_probability",
     unit: "%",
   },
+  vacant_house_id: {
+    label: "vacant_house_id",
+  },
+  vacant_house_address: {
+    label: "vacant_house_address",
+  },
+  vacant_house_longitude: {
+    label: "vacant_house_longitude",
+  },
+  vacant_house_latitude: {
+    label: "vacant_house_latitude",
+  },
+  vacant_house_source_info: {
+    label: "vacant_house_source_info",
+  },
+  geocoded_address: {
+    label: "geocoded_address",
+  },
+  geocoded_latitude: {
+    label: "geocoded_latitude",
+  },
+  geocoded_longitude: {
+    label: "geocoded_longitude",
+  },
+  geocoding_source_info: {
+    label: "geocoding_source_info",
+  },
+  has_water_supply: {
+    label: "has_water_supply",
+  },
+  has_juki_registry: {
+    label: "has_juki_registry",
+  },
+  has_touki_registry: {
+    label: "has_touki_registry",
+  },
+  has_juki_and_water: {
+    label: "has_juki_and_water",
+  },
+  has_vacant_result: {
+    label: "has_vacant_result",
+  },
+  has_juki_water_property: {
+    label: "has_juki_water_property",
+  },
+  has_geocoding: {
+    label: "has_geocoding",
+  },
+  has_juki_water_property_vacant: {
+    label: "has_juki_water_property_vacant",
+  },
+  // index_right: {
+  //   label: "index_right",
+  // },
   created_at: {
     label: "created_at",
   },
