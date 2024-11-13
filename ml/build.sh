3816158fc753627a4d9f5d95b9ce1affc404481d
HideBa <49897538+HideBa@users.noreply.github.com>
2024-11-13 21:05:07 +0100

    chore: change build script for pyinstaller (#424)

--- ml/build.sh
+++ ml/build.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
 
-DEMO_DIR="./src/_demo"
 TEMP_DIR="./dist/.temp"
+DIST_DIR="./dist"
+
+# List of files to build
+FILES=(
+    "./async_tasks/IF001.py"
+    "./async_tasks/IF003.py"
+    "./async_tasks/IF004.py"
+)
+
+# Generate collect-all options from requirements.txt
+COLLECT_ALL_OPTS=()
+while IFS= read -r line; do
+    package=$(echo "$line" | cut -d'=' -f1)
+    COLLECT_ALL_OPTS+=("--collect-all" "$package")
+done <requirements.txt
 
 # ビルドディレクトリのクリーニング
 rm -rf dist
 mkdir -p dist $TEMP_DIR
 
-# src ディレクトリ内のすべての .py ファイルをビルド
-for py_file in $DEMO_DIR/*.py; do
+# Build each file in the list
+for py_file in "${FILES[@]}"; do
     # ファイル名のみを抽出（拡張子なし）
     base_name=$(basename "$py_file" .py)
 
-    # PyInstallerでビルド
-    pyinstaller --onefile --distpath $TEMP_DIR/dist --workpath $TEMP_DIR/build --specpath $TEMP_DIR "$py_file"
+    # I want to run these kind of command
+    # pyinstaller --onefile --collect-all chardet --collect-all pandas --collect-all geopandas --collect-all pyogrio --add-data "async_tasks:async_tasks" --add-data "src;src" --paths=./src --name IF001 ./async_tasks/IF001.py
 
-    # ビルドされたバイナリを binaries ディレクトリに移動
-    mv "$TEMP_DIR/dist/$base_name" dist/
+    poetry run pyinstaller --onefile \
+        --distpath $DIST_DIR \
+        "${COLLECT_ALL_OPTS[@]}" \
+        --add-data="async_tasks:async_tasks" \
+        --add-data="src:src" \
+        --paths="./src" \
+        --name "$base_name" \
+        "$py_file"
+
 done
-
-# 一時ファイルの削除
-rm -rf $TEMP_DIR
