b6b3224aa55749d071091cf01b8db0042f65ffbe
m-abe-dev <uuu.theman.jp@gmail.com>
2024-11-19 19:29:27 +0100

    add: react hook form

--- app/src/pages/evaluation/index.tsx
+++ app/src/pages/evaluation/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   Card,
   makeStyles,
@@ -10,7 +11,7 @@ import {
 } from "@fluentui/react-components";
 import { DeleteRegular, Dismiss24Regular } from "@fluentui/react-icons";
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useForm, type SubmitHandler } from "react-hook-form";
 import { DialogSurface } from "../../components/ui/dialog-surface";
 import { DialogBody } from "../../components/ui/dialog-body";
 import { DialogTitle } from "../../components/ui/dialog-title";
@@ -117,33 +118,47 @@ const initialAdvancedSettings: AdvancedSettingsType = {
   similarityThreshold: 0, // デフォルト値
 };
 
-export const JobEvaluation = (): JSX.Element => {
-  const navigate = useNavigate();
-  const styles = useStyles();
-  const { isOpen, setIsOpen } = useDialogState(false);
+type FormValues = {
+  selectedModelFile: SelectModelFile | null;
+  selectedFile: SelectNormalizedDataSet | null;
+  selectedAreaFile: SelectRawDataSet | null;
+  selectedAreaIdColumn: string;
+  selectedAreaNameColumn: string;
+  advancedSettings: AdvancedSettingsType;
+};
 
-  // 選択されたファイルの状態管理
-  const [selectedFile, setSelectedFile] =
-    useState<SelectNormalizedDataSet | null>(null);
-  const [selectedModelFile, setSelectedModelFile] =
-    useState<SelectModelFile | null>(null);
-  const [selectedAreaFile, setSelectedAreaFile] =
-    useState<SelectRawDataSet | null>(null);
+export const JobEvaluation = (): JSX.Element => {
+  const styles = useStyles();
+  const navigate = useNavigate();
+
+  // フォームの初期化
+  const { handleSubmit, setValue, watch } = useForm<FormValues>({
+    defaultValues: {
+      selectedModelFile: null,
+      selectedFile: null,
+      selectedAreaFile: null,
+      selectedAreaIdColumn: "",
+      selectedAreaNameColumn: "",
+      advancedSettings: initialAdvancedSettings,
+    },
+  });
 
   // ダイアログの状態管理
   const importModelDatasetDialogState = useDialogState();
   const importAnalysisDatasetDialogState = useDialogState();
   const importAreaDatasetDialogState = useDialogState();
-
-  // 高度な設定の状態管理
-  const [advancedSettings, setAdvancedSettings] =
-    useState<AdvancedSettingsType>(initialAdvancedSettings);
+  const analysisStartDialogState = useDialogState(false);
 
   // カラム情報と選択されたカラムの状態管理
   const [areaColumns, setAreaColumns] = useState<string[]>([]);
-  const [selectedAreaIdColumn, setSelectedAreaIdColumn] = useState<string>("");
-  const [selectedAreaNameColumn, setSelectedAreaNameColumn] =
-    useState<string>("");
+
+  // 選択された値を取得
+  const selectedModelFile = watch("selectedModelFile");
+  const selectedFile = watch("selectedFile");
+  const selectedAreaFile = watch("selectedAreaFile");
+  const selectedAreaIdColumn = watch("selectedAreaIdColumn");
+  const selectedAreaNameColumn = watch("selectedAreaNameColumn");
+  const advancedSettings = watch("advancedSettings");
 
   // カラム情報を取得するフック
   const { data: areaFileColumns } = useFetchDatasetColumns({
@@ -158,212 +173,229 @@ export const JobEvaluation = (): JSX.Element => {
     }
   }, [areaFileColumns]);
 
+  // フォーム送信時の処理
+  const onSubmit: SubmitHandler<FormValues> = async (data) => {
+    // ここで API リクエストを行う
+  };
+
   // 分析対象のデータの削除
   const handleRemoveFile = (): void => {
-    setSelectedFile(null);
+    setValue("selectedFile", null);
   };
 
   // モデルファイルの削除
   const handleRemoveModelFile = (): void => {
-    setSelectedModelFile(null);
+    setValue("selectedModelFile", null);
   };
 
   // 地域集計用データの削除
   const handleRemoveAreaFile = (): void => {
-    setSelectedAreaFile(null);
+    setValue("selectedAreaFile", null);
     setAreaColumns([]);
-    setSelectedAreaIdColumn("");
-    setSelectedAreaNameColumn("");
+    setValue("selectedAreaIdColumn", "");
+    setValue("selectedAreaNameColumn", "");
   };
 
   return (
-    <div className={styles.root}>
-      <h2 className={styles.heading}>空き家判定</h2>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className={styles.root}>
+        <h2 className={styles.heading}>空き家判定</h2>
 
-      <div className={styles.contents}>
-        {/* モデルファイルの選択 */}
-        <Card>
-          <Subtitle2>① 利用するモデルを選択</Subtitle2>
-          <div className={styles.file}>
-            {selectedModelFile ? (
-              <>
-                <span className={styles.fileName}>
-                  {selectedModelFile.file_name}
-                </span>
-                <span
-                  className={styles.deleteIconWrapper}
-                  onClick={handleRemoveModelFile}
+        <div className={styles.contents}>
+          {/* モデルファイルの選択 */}
+          <Card>
+            <Subtitle2>① 利用するモデルを選択</Subtitle2>
+            <div className={styles.file}>
+              {selectedModelFile ? (
+                <>
+                  <span className={styles.fileName}>
+                    {selectedModelFile.file_name}
+                  </span>
+                  <span
+                    className={styles.deleteIconWrapper}
+                    onClick={handleRemoveModelFile}
+                  >
+                    <DeleteRegular fontSize={16} />
+                  </span>
+                </>
+              ) : (
+                <Button
+                  appearance="primary"
+                  onClick={() => importModelDatasetDialogState.setIsOpen(true)}
                 >
-                  <DeleteRegular fontSize={16} />
-                </span>
-              </>
-            ) : (
-              <Button
-                appearance="primary"
-                onClick={() => importModelDatasetDialogState.setIsOpen(true)}
-              >
-                選択
-              </Button>
-            )}
-          </div>
-        </Card>
+                  選択
+                </Button>
+              )}
+            </div>
+          </Card>
 
-        {/* ダイアログの定義 */}
-        <DialogSelectDataset<SelectModelFile>
-          dialogState={importModelDatasetDialogState}
-          emptyMessage="現在表示できるモデルはありません"
-          isModel
-          onSelected={(data) => {
-            setSelectedModelFile(data);
-          }}
-          placeholder="モデル名"
-          title="利用するモデルを選択"
-          useFetchDatasets={useFetchModelFiles}
-        />
+          {/* ダイアログの定義 */}
+          <DialogSelectDataset<SelectModelFile>
+            dialogState={importModelDatasetDialogState}
+            emptyMessage="現在表示できるモデルはありません"
+            isModel
+            onSelected={(data) => {
+              setValue("selectedModelFile", data);
+            }}
+            placeholder="モデル名"
+            title="利用するモデルを選択"
+            useFetchDatasets={useFetchModelFiles}
+          />
 
-        {/* 分析対象のデータの選択 */}
-        <Card>
-          <Subtitle2>② 分析対象のデータを選択</Subtitle2>
-          <div className={styles.file}>
-            {selectedFile ? (
-              <div key={selectedFile.id} className={styles.fileItem}>
-                <span className={styles.fileName}>
-                  {selectedFile.file_name}
-                </span>
-                <span
-                  className={styles.deleteIconWrapper}
-                  onClick={handleRemoveFile}
+          {/* 分析対象のデータの選択 */}
+          <Card>
+            <Subtitle2>② 分析対象のデータを選択</Subtitle2>
+            <div className={styles.file}>
+              {selectedFile ? (
+                <div key={selectedFile.id} className={styles.fileItem}>
+                  <span className={styles.fileName}>
+                    {selectedFile.file_name}
+                  </span>
+                  <span
+                    className={styles.deleteIconWrapper}
+                    onClick={handleRemoveFile}
+                  >
+                    <DeleteRegular fontSize={16} />
+                  </span>
+                </div>
+              ) : (
+                <Button
+                  appearance="primary"
+                  onClick={() =>
+                    importAnalysisDatasetDialogState.setIsOpen(true)
+                  }
                 >
-                  <DeleteRegular fontSize={16} />
-                </span>
+                  選択
+                </Button>
+              )}
+            </div>
+          </Card>
+
+          {/* ダイアログの定義 */}
+          <DialogSelectDataset<SelectNormalizedDataSet>
+            dialogState={importAnalysisDatasetDialogState}
+            emptyMessage="現在表示できるデータセットはありません"
+            onSelected={(data) => {
+              setValue("selectedFile", data);
+            }}
+            placeholder="データ名"
+            title="分析対象のデータを選択"
+            useFetchDatasets={useFetchNormalizedDatasets}
+          />
+
+          {/* 地域集計用データの選択 */}
+          <Card>
+            <Subtitle2>③ 地域集計用データをアップロード</Subtitle2>
+            <div className={styles.file}>
+              {selectedAreaFile ? (
+                <div key={selectedAreaFile.id} className={styles.fileItem}>
+                  <span className={styles.fileName}>
+                    {selectedAreaFile.file_name}
+                  </span>
+                  <span
+                    className={styles.deleteIconWrapper}
+                    onClick={handleRemoveAreaFile}
+                  >
+                    <DeleteRegular fontSize={16} />
+                  </span>
+                </div>
+              ) : (
+                <Button
+                  appearance="primary"
+                  onClick={() => importAreaDatasetDialogState.setIsOpen(true)}
+                >
+                  選択
+                </Button>
+              )}
+            </div>
+
+            {/* ドロップダウンの表示 */}
+            {selectedAreaFile && (
+              <div className={styles.dropdownWrapper}>
+                <label htmlFor="area-id-dropdown">地域IDカラム</label>
+                <Dropdown
+                  className={styles.dropdown}
+                  id="area-id-dropdown"
+                  onOptionSelect={(event, data) =>
+                    setValue("selectedAreaIdColumn", data.optionValue ?? "")
+                  }
+                  placeholder="選択"
+                  value={selectedAreaIdColumn}
+                >
+                  {areaColumns.map((column) => (
+                    <Option key={column} text={column} value={column}>
+                      {column}
+                    </Option>
+                  ))}
+                </Dropdown>
+                <label htmlFor="area-name-dropdown">地域名称カラム</label>
+                <Dropdown
+                  className={styles.dropdown}
+                  id="area-name-dropdown"
+                  onOptionSelect={(event, data) =>
+                    setValue("selectedAreaNameColumn", data.optionValue ?? "")
+                  }
+                  placeholder="選択"
+                  value={selectedAreaNameColumn}
+                >
+                  {areaColumns.map((column) => (
+                    <Option key={column} text={column} value={column}>
+                      {column}
+                    </Option>
+                  ))}
+                </Dropdown>
               </div>
-            ) : (
-              <Button
-                appearance="primary"
-                onClick={() => importAnalysisDatasetDialogState.setIsOpen(true)}
-              >
-                選択
-              </Button>
             )}
-          </div>
-        </Card>
+          </Card>
 
-        {/* ダイアログの定義 */}
-        <DialogSelectDataset<SelectNormalizedDataSet>
-          dialogState={importAnalysisDatasetDialogState}
-          emptyMessage="現在表示できるデータセットはありません"
-          onSelected={(data) => {
-            setSelectedFile(data);
-          }}
-          placeholder="データ名"
-          title="分析対象のデータを選択"
-          useFetchDatasets={useFetchNormalizedDatasets}
-        />
+          {/* ダイアログの定義 */}
+          <DialogSelectDataset<SelectRawDataSet>
+            dialogState={importAreaDatasetDialogState}
+            emptyMessage="現在表示できるデータセットはありません"
+            onSelected={(data) => {
+              setValue("selectedAreaFile", data);
+            }}
+            placeholder="データ名"
+            title="地域集計用データを選択"
+            useFetchDatasets={useFetchRawDatasets}
+          />
 
-        {/* 地域集計用データの選択 */}
-        <Card>
-          <Subtitle2>③ 地域集計用データをアップロード</Subtitle2>
-          <div className={styles.file}>
-            {selectedAreaFile ? (
-              <div key={selectedAreaFile.id} className={styles.fileItem}>
-                <span className={styles.fileName}>
-                  {selectedAreaFile.file_name}
-                </span>
-                <span
-                  className={styles.deleteIconWrapper}
-                  onClick={handleRemoveAreaFile}
-                >
-                  <DeleteRegular fontSize={16} />
-                </span>
-              </div>
-            ) : (
-              <Button
-                appearance="primary"
-                onClick={() => importAreaDatasetDialogState.setIsOpen(true)}
-              >
-                選択
-              </Button>
-            )}
-          </div>
+          {/* 高度な設定 */}
+          <Card>
+            <Subtitle2>④ 高度な設定</Subtitle2>
+            <div className={styles.file}>
+              <DialogSetting
+                onChange={(newValue) => setValue("advancedSettings", newValue)}
+                value={advancedSettings}
+              />
+            </div>
+          </Card>
+        </div>
 
-          {/* ドロップダウンの表示 */}
-          <div className={styles.dropdownWrapper}>
-            <label htmlFor="area-id-dropdown">地域IDカラム</label>
-            <Dropdown
-              className={styles.dropdown}
-              id="area-id-dropdown"
-              onOptionSelect={(event, data) =>
-                setSelectedAreaIdColumn(data.optionValue ?? "")
-              }
-              placeholder="選択"
-              value={selectedAreaIdColumn}
-            >
-              {areaColumns.map((column) => (
-                <Option key={column} text={column} value={column}>
-                  {column}
-                </Option>
-              ))}
-            </Dropdown>
-            <label htmlFor="area-name-dropdown">地域名称カラム</label>
-            <Dropdown
-              className={styles.dropdown}
-              id="area-name-dropdown"
-              onOptionSelect={(event, data) =>
-                setSelectedAreaIdColumn(data.optionValue ?? "")
-              }
-              placeholder="選択"
-              value={selectedAreaNameColumn}
-            >
-              {areaColumns.map((column) => (
-                <Option key={column} text={column} value={column}>
-                  {column}
-                </Option>
-              ))}
-            </Dropdown>
-          </div>
-        </Card>
+        {/* フッター */}
+        <div className={styles.footer}>
+          <Button
+            className={styles.restartButton}
+            disabled={
+              !selectedFile ||
+              !selectedModelFile ||
+              !selectedAreaFile ||
+              !selectedAreaIdColumn ||
+              !selectedAreaNameColumn
+            }
+            type="submit"
+          >
+            分析開始
+          </Button>
+        </div>
 
-        {/* ダイアログの定義 */}
-        <DialogSelectDataset<SelectRawDataSet>
-          dialogState={importAreaDatasetDialogState}
-          emptyMessage="現在表示できるデータセットはありません"
-          onSelected={(data) => {
-            setSelectedAreaFile(data);
-          }}
-          placeholder="データ名"
-          title="地域集計用データを選択"
-          useFetchDatasets={useFetchRawDatasets}
-        />
-
-        {/* 高度な設定 */}
-        <Card>
-          <Subtitle2>④ 高度な設定</Subtitle2>
-          <div className={styles.file}>
-            <DialogSetting
-              onChange={(newValue) => setAdvancedSettings(newValue)}
-              value={advancedSettings}
-            />
-          </div>
-        </Card>
-      </div>
-
-      {/* フッター */}
-      <div className={styles.footer}>
+        {/* 分析開始後のダイアログ */}
         <Dialog
-          onOpenChange={(event, data) => setIsOpen(data.open)}
-          open={isOpen}
+          onOpenChange={(event, data) =>
+            analysisStartDialogState.setIsOpen(data.open)
+          }
+          open={analysisStartDialogState.isOpen}
         >
-          <DialogTrigger disableButtonEnhancement>
-            <Button
-              className={styles.restartButton}
-              disabled={
-                !selectedFile || !selectedModelFile || !selectedAreaFile
-              }
-              onClick={() => setIsOpen(true)}
-            >
-              分析開始
-            </Button>
-          </DialogTrigger>
           <DialogSurface className={styles.dialogSurface}>
             <DialogBody>
               <DialogTitle
@@ -378,7 +410,7 @@ export const JobEvaluation = (): JSX.Element => {
                           strokeWidth={2}
                         />
                       }
-                      onClick={() => setIsOpen(false)}
+                      onClick={() => analysisStartDialogState.setIsOpen(false)}
                     />
                   </DialogTrigger>
                 }
@@ -395,7 +427,7 @@ export const JobEvaluation = (): JSX.Element => {
                 <Button
                   appearance="primary"
                   onClick={() => {
-                    setIsOpen(false);
+                    analysisStartDialogState.setIsOpen(false);
                     navigate("/job");
                   }}
                   size="medium"
@@ -407,6 +439,6 @@ export const JobEvaluation = (): JSX.Element => {
           </DialogSurface>
         </Dialog>
       </div>
-    </div>
+    </form>
   );
 };
