a4151afba4e3262f622b8ee71f174c82499a0996
ksyunnnn <is.syunsukekobashi@gmail.com>
2024-11-19 17:16:16 +0900

    resultの型定義を追加できるところだけ追加

--- app/src/@types/job-task-result.ts
+++ app/src/@types/job-task-result.ts
@@ -1,1 +1,16 @@
-export type JobTaskResult = Record<string, string>;
+/** @todo たぶんpreprocess_typeごとに違う？ */
+type PreprocessResult = Record<string, string>;
+
+type ModelCreateResult = {
+  accuracy: string; // 正解率
+  f1Score: string; // f値
+  specificity: string; // 特異度
+  precision: string; // 適合率
+  recall: string; // 再現率
+  important_columns: { column: string; value: string }[];
+};
+
+/** @todo / 命名のリファクタも */
+type ResultResult = Record<string, string>;
+
+export type JobTaskResult = PreprocessResult | ModelCreateResult | ResultResult;
