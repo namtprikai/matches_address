eb163b0bba8c9f2752b0eccab8d77b3bfc16717a
kimizuy <kimizuyamasaki@gmail.com>
2024-11-19 20:16:02 -0800

    update saveDataSetFile

--- app/src/utils/save-data-set-file.ts
+++ app/src/utils/save-data-set-file.ts
@@ -1,17 +1,24 @@
-export async function saveDataSetFile(file: File | undefined): Promise<void> {
+import { type InsertRawDataSet } from "../schema";
+
+export async function saveDataSetFile(
+  file: File | undefined,
+): Promise<{ insertedId: InsertRawDataSet["id"] } | undefined> {
   if (!file) return;
   const ext = file.name.split(".").pop();
   if (!ext) return;
   const uuid = crypto.randomUUID();
   const file_path = `${uuid}.${ext}`;
-  await window.ipcRenderer.invoke("insertRawDatasets", {
-    file_name: file.name,
-    file_path,
-  });
   const arrayBuffer = await file.arrayBuffer();
   // TODO: サイズが大きいファイルのためにパフォーマンス改善が必要かも
   await window.ipcRenderer.invoke("writeDatasetFile", {
     data: arrayBuffer,
     fileName: file_path,
   });
+
+  const result = await window.ipcRenderer.invoke("insertRawDatasets", {
+    file_name: file.name,
+    file_path,
+  });
+
+  return result;
 }
