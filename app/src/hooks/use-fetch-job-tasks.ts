167c6324e898657a64df3a100edc49708080ee3e
ksyunnnn <is.syunsukekobashi@gmail.com>
2024-11-22 17:21:39 +0900

    モデル精度表示のデータつなぎこみ・resultの型を判別するための制御を追加

--- app/src/hooks/use-fetch-job-tasks.ts
+++ app/src/hooks/use-fetch-job-tasks.ts
@@ -7,6 +7,30 @@ type Params = {
 
 const fetcher = async ({ jobId }: Params): Promise<SelectJobTask[]> => {
   const result = await window.ipcRenderer.invoke("selectJobTasks", jobId);
+
+  /**
+   * job_tasks.result に入っているJsonの型が取得時に判別できないためここで設定
+   * Python側で詰めてもらうほうが良いかもしれないが一旦
+   * 設定の基準: task.preprocess_type があるかどうか.現時点では判別可能
+   */
+  result.forEach((task, index) => {
+    if (task.preprocess_type) {
+      result[index].result = {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment -- 理由は上段の通り
+        // @ts-ignore
+        taskResultType: "preprocess",
+        ...result[index].result,
+      };
+    } else {
+      result[index].result = {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment -- 理由は上段の通り
+        // @ts-ignore
+        taskResultType: "model_create",
+        ...result[index].result,
+      };
+    }
+  });
+
   return result;
 };
 
