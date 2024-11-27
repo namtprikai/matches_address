1ae4c669991d742565d35635350566cb9a0f8ffc
ksyunnnn <is.syunsukekobashi@gmail.com>
2024-11-27 17:49:47 +0900

    プロダクション不要コードを削除・呼び出しバイナリファイルのタイポを修正

--- app/src/pages/normalization/index.tsx
+++ app/src/pages/normalization/index.tsx
@@ -56,19 +56,6 @@ export function Normalization(): JSX.Element {
   );
 
   const handleClick = async (): Promise<void> => {
-    // TODO: jobの登録はpython側で行うため後で削除する
-    await window.ipcRenderer.invoke("_debugInsertJobs", {
-      type: "preprocess",
-      parameters: {
-        ...parameters,
-        // TODO: 型エラーを直すために一時的に追加しただけ
-        database_path: "test.db",
-        input_path: "test.csv",
-      },
-      is_named: false,
-    });
-
-    // TODO: parametersが正しいかどうかのバリデーションを行う必要がある
     await window.ipcRenderer.invoke("execE001", {
       parameters,
     });
