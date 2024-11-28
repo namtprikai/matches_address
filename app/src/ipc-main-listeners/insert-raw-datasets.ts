2e4b5c71c34a329f79965eaf031500e6f0244282
kimizuy <kimizuyamasaki@gmail.com>
2024-11-27 19:48:46 -0800

    refactor arguments

--- app/src/ipc-main-listeners/insert-raw-datasets.ts
+++ app/src/ipc-main-listeners/insert-raw-datasets.ts
@@ -4,14 +4,14 @@ import { type IpcMainListener } from ".";
 
 export const insertRawDatasets = (async (
   _: unknown,
-  { file_name, file_path }: InsertRawDataSet,
+  values: {
+    file_name: InsertRawDataSet["file_name"];
+    file_path: InsertRawDataSet["file_path"];
+  },
 ): Promise<{ insertedId: number }> => {
   const res = await db
     .insert(raw_data_sets)
-    .values({
-      file_name,
-      file_path,
-    })
+    .values(values)
     .returning({
       insertedId: result_views.id,
     })
