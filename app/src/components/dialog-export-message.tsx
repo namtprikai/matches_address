afe7f0cee7e34bc2766d5ef2d24f9c3d25104735
kimizuy <kimizuyamasaki@gmail.com>
2024-12-18 13:12:22 +0900

    add DialogExportMessage

---
+++ app/src/components/dialog-export-message.tsx
@@ -0,0 +1,41 @@
+import { Dialog, Body1 } from "@fluentui/react-components";
+import { type ReturnUseDialogState } from "../hooks/use-dialog-state";
+import { Button } from "./ui/button";
+import { DialogSurface } from "./ui/dialog-surface";
+import { DialogBody } from "./ui/dialog-body";
+import { DialogTitle } from "./ui/dialog-title";
+import { DialogContent } from "./ui/dialog-content";
+import { DialogActions } from "./ui/dialog-actions";
+
+type Props = {
+  dialogState: ReturnUseDialogState;
+};
+
+export const DialogExportMessage = ({ dialogState }: Props): JSX.Element => {
+  const { isOpen: isDialogOpen, setIsOpen: setIsDialogOpen } = dialogState;
+
+  return (
+    <Dialog
+      onOpenChange={(_, { open }) => setIsDialogOpen(open)}
+      open={isDialogOpen}
+    >
+      <DialogSurface>
+        <DialogBody>
+          <DialogTitle>ダウンロード処理を開始しました</DialogTitle>
+          <DialogContent>
+            <Body1>
+              ダウンロードの準備が完了するまで一定の時間がかかります
+              <br />
+              ステータスは「非同期処理一覧画面」で確認できます
+            </Body1>
+          </DialogContent>
+          <DialogActions>
+            <a href="#job">
+              <Button appearance="primary">非同期処理一覧画面へ</Button>
+            </a>
+          </DialogActions>
+        </DialogBody>
+      </DialogSurface>
+    </Dialog>
+  );
+};
