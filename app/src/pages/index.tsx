b6abc7a79bc3d538ab1dc8430ea1fd15346b62e3
ksyunnnn <is.syunsukekobashi@gmail.com>
2024-12-19 15:05:06 +0900

    非同期処理結果・判定結果画面について詳細ページを追加

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -24,6 +24,7 @@ import { MlDetail } from "./job/detail/ml";
 import { Normalization } from "./normalization";
 import { JobEvaluation } from "./evaluation";
 import { ExportDetail } from "./job/detail/export";
+import { ResultDetail } from "./job/detail/result";
 
 // クライアントだけで動作するアプリケーションのため`createHashRouter`を使用する
 const router = createHashRouter([
@@ -66,6 +67,10 @@ const router = createHashRouter([
           {
             path: "export",
             element: <ExportDetail />,
+          },
+          {
+            path: "result",
+            element: <ResultDetail />,
           },
         ],
       },
