cced2f3aea78c8f2de2fd2fe27cdd5cc3dca47d0
khailv <khai.le@rikai.technology>
2024-12-24 13:41:40 +0700

    update lib

--- ml/build.sh
+++ ml/build.sh
@@ -9,7 +9,7 @@ mkdir -p $DIST_DIR
 
 poetry run pyinstaller --onefile --distpath $DIST_DIR --collect-all chardet --collect-all pandas --collect-all geopandas --collect-all pyogrio --collect-all shapely --add-data "src:src" --paths="$SRC_DIR" --name IF001 ./async_tasks/IF001.py
 
-poetry run pyinstaller --onefile --distpath $DIST_DIR --collect-all japanize_matplotlib --collect-all memory_profiler --collect-all chardet --collect-all pandas --collect-all sklearn --collect-all lightgbm --collect-all numpy --collect-all optuna --collect-all seaborn --collect-all japanize_matplotlib --add-data "src:src" --paths="$SRC_DIR" --name IF002 ./async_tasks/IF002.py
+poetry run pyinstaller --onefile --distpath $DIST_DIR --collect-all imblearn --collect-all japanize_matplotlib --collect-all memory_profiler --collect-all chardet --collect-all pandas --collect-all sklearn --collect-all lightgbm --collect-all numpy --collect-all optuna --collect-all seaborn --collect-all japanize_matplotlib --add-data "src:src" --paths="$SRC_DIR" --name IF002 ./async_tasks/IF002.py
 
 poetry run pyinstaller --onefile --distpath $DIST_DIR --collect-all chardet --collect-all pandas --collect-all geopandas --collect-all shapely --collect-all lightgbm --collect-all numpy --add-data "async_tasks:async_tasks" --add-data "src:src" --paths="$SRC_DIR" --name IF003 ./async_tasks/IF003.py
 
