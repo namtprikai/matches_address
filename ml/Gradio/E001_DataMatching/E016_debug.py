f4191d8b3fe079d4bd268fdf64922862774180b4
Hiroaki Sengoku <949401+xianshiyuming@users.noreply.github.com>
2025-01-04 03:04:49 +0900

    temp update for test

--- ml/Gradio/E001_DataMatching/E016_debug.py
+++ ml/Gradio/E001_DataMatching/E016_debug.py
@@ -191,10 +191,11 @@ def generate_file_paths(citycode_value, targetyear_value):
     """
     
     e14_merged_file = f'./data/{citycode_value}/E014/outputs/matched_data.csv'
-    #tatemono_file = f'./data/{citycode_value}/E016/inputs/toyota_lod0_attributes.csv'
+    #tatemono_file = f'./data/{citycode_value}/E016/inputs/PLATEAU_建物データ.csv'
+    tatemono_file = f'./data/{citycode_value}/E016/inputs/PLATEAU_建物データ.gpkg'
     # citygml format
     #tatemono_file = f'./data/{citycode_value}/E016/inputs/PLATEAU_建物データ_citygml_豊田市_アプリ読込用.zip'
-    tatemono_file = f'./data/{citycode_value}/E016/inputs/PLATEAU_建物データ_citygml_豊橋市_アプリ読込用.zip'
+    #tatemono_file = f'./data/{citycode_value}/E016/inputs/PLATEAU_建物データ_citygml_豊橋市_アプリ読込用.zip'
     shp_file = f'./data/{citycode_value}/E016/inputs/r2ka23.gpkg'
     output_path = f'./data/{citycode_value}/E016/outputs/D901_{targetyear_value}.csv'
     return tatemono_file, e14_merged_file, shp_file, output_path
