dbb365115b649b20c767bb94806b1953f29cde39
ksyunnnn <is.syunsukekobashi@gmail.com>
2025-01-04 14:54:55 +0900

    ビューの上限を修正

--- app/src/components/result-sheet.tsx
+++ app/src/components/result-sheet.tsx
@@ -55,6 +55,8 @@ export const ResultSheet = ({ sheetId }: Props): JSX.Element => {
   const resultViewsGridTemplate = (() => {
     if (!data) return "";
     switch (data.length) {
+      case 1:
+        return "";
       case 2:
         return styles.template2th;
       case 3:
@@ -62,7 +64,7 @@ export const ResultSheet = ({ sheetId }: Props): JSX.Element => {
       case 4:
         return styles.template4th;
       default:
-        return "";
+        return styles.template4th;
     }
   })();
 
