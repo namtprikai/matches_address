8541cb9bf5788f47c41fc6eaba7780f7a53109b5
khailv <khai.le@rikai.technology>
2025-01-04 11:56:04 +0700

    rename building id

--- ml/src/E001_DataMatching/E016.py
+++ ml/src/E001_DataMatching/E016.py
@@ -333,6 +333,8 @@ def load_and_process_data(file_path, crs, geometry, file_type, data_type):
         # 無効なジオメトリを除外
         df = df[df['geometry'].notnull()]
         if data_type == 'plateau':
+            if 'buildingID' not in df.columns:
+                df['buildingID'] = df.index + 1
             df['building_id'] = df['buildingID'].astype(str)
         else:
             if 'building_id' not in df.columns:
@@ -371,6 +373,8 @@ def load_and_process_data(file_path, crs, geometry, file_type, data_type):
 
         # buildingID列を追加
         if data_type == 'plateau':
+            if 'buildingID' not in gdf.columns:
+                gdf['buildingID'] = gdf.index + 1
             gdf['building_id'] = gdf['buildingID'].astype(str)
         else:
             if 'building_id' not in gdf.columns:
@@ -389,6 +393,8 @@ def load_and_process_data(file_path, crs, geometry, file_type, data_type):
 
         # buildingID列を追加
         if data_type == 'plateau':
+            if 'buildingID' not in gdf.columns:
+                gdf['buildingID'] = gdf.index + 1
             gdf['building_id'] = gdf['buildingID'].astype(str)
         else:
             if 'building_id' not in gdf.columns:
