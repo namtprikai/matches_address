14199a55f4441fc888ce9976aeefb7798bbf9c33
kimizuy <kimizuyamasaki@gmail.com>
2025-01-15 17:19:24 +0900

    refactor: improve type

--- app/src/components/bi/map/map-component/index.tsx
+++ app/src/components/bi/map/map-component/index.tsx
@@ -37,7 +37,7 @@ export const PREDICTED_PROBABILITY: Record<
   },
 };
 
-const INITIAL_CENTER: [number, number] = [139.7671, 35.6812]; // 東京駅
+const INITIAL_CENTER: LngLatLike = [139.7671, 35.6812]; // 東京駅
 
 const useMapComponentStyles = makeStyles({
   map: {
