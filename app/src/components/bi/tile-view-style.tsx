3d37fd59459cf21a57a08a9df1d937a22b43a9dc
ksyunnnn <is.syunsukekobashi@gmail.com>
2025-02-05 16:52:37 +0900

    コンポーネントのSwichを修正

--- app/src/components/bi/tile-view-style.tsx
+++ app/src/components/bi/tile-view-style.tsx
@@ -14,270 +14,92 @@ type Props = {
   view: View;
 };
 
-/**
- * このコンポーネント内で同様の処理をまとめているだけなため、後で削除することも検討する
- */
-const pickArgsFromParameters = (
-  parameters: SelectResultView["parameters"],
-  style: Exclude<SelectResultView["style"], "table" | "map" | null>,
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type -- 型推論を利用したいため
-) => {
-  const xAxis = parameters.find((p) =>
-    style === "pie" ? p.key === "label" : p.key === "xAxis",
-  );
-  const yAxis = parameters.find((p) =>
-    style === "pie" ? p.key === "value" : p.key === "yAxis",
-  );
-
-  const groupingParameters = parameters.filter((p) => p.type === "group");
-
-  const filterParameters = parameters.filter(
-    (p) => p.type === "filter" && p.key !== "year" && p.key !== "area",
-  );
-
-  const yearParameter = parameters.find(
-    (p) => p.key === "year" && p.type === "filter",
-  );
-
-  const groupingCalc = parameters.find(
-    (p) => p.key === "group_calc" && p.type === "group_option",
-  );
-
-  const areaParameter = parameters.find(
-    (p) => p.key === "area" && p.type === "filter",
-  );
-
-  return {
-    xAxis,
-    yAxis,
-    groupingParameters,
-    filterParameters,
-    yearParameter,
-    areaParameter,
-    groupingCalc,
-  };
-};
-
 export const TileViewStyle = ({ view }: Props): JSX.Element => {
-  /** 仮 */
-  const { style, unit: type, parameters, dataSetResultId: resultId } = view;
-  if (style === "pie") {
-    const {
-      xAxis,
-      yAxis,
-      groupingParameters,
-      filterParameters,
-      yearParameter,
-      areaParameter: areaParameter,
-      groupingCalc,
-    } = pickArgsFromParameters(parameters, style);
+  const { style, unit, parameters, dataSetResultId } = view;
 
-    if (!xAxis || !yAxis) {
-      return <div>パラメーターの値を正しく設定してください</div>;
-    }
-
-    if (type === "building") {
+  switch (true) {
+    case style === "pie" && unit === "building":
       return (
         <div>
-          <ChartPie
-            view={{
-              dataSetResultId: resultId,
-              unit: type,
-              style: "pie",
-              title: "",
-              parameters,
-            }}
-          />
+          <ChartPie view={view} />
         </div>
       );
-    }
-
-    return (
-      <div>
-        <ChartPie
-          filterByAreas={areaParameter?.value}
-          filterByYear={{
-            startValue: yearParameter?.value?.start,
-            endValue: yearParameter?.value?.end,
-          }}
-          filterConditions={filterParameters.flatMap((p) => p.value)}
-          groupingCalc={groupingCalc?.value as "avg" | "sum"}
-          groupingConditions={groupingParameters.map((p) => p.value)}
-          resultId={resultId}
-          type={type}
-          x={xAxis.value as AREA_DATASET_COLUMN}
-          y={yAxis.value as AREA_DATASET_COLUMN}
-        />
-      </div>
-    );
-  }
-
-  if (style === "bar") {
-    const {
-      xAxis,
-      yAxis,
-      groupingParameters,
-      filterParameters,
-      yearParameter,
-      areaParameter: areaParameter,
-      groupingCalc,
-    } = pickArgsFromParameters(parameters, style);
-
-    if (!xAxis || !yAxis) {
-      return <div>パラメーターの値を正しく設定してください</div>;
-    }
-
-    if (view.style === "bar") {
+    case style === "bar" && unit === "area":
       return (
         <div>
-          <ChartBar
-            view={{
-              dataSetResultId: resultId,
-              unit: type,
-              style: "bar",
-              title: "",
-              parameters,
-            }}
-          />
+          <ChartBar view={view} />
         </div>
       );
-    }
-
-    return (
-      <div>
-        <ChartBar
-          filterByAreas={areaParameter?.value}
-          filterByYear={{
-            startValue: yearParameter?.value?.start,
-            endValue: yearParameter?.value?.end,
-          }}
-          filterConditions={filterParameters.flatMap((p) => p.value)}
-          groupingCalc={groupingCalc?.value as "avg" | "sum"}
-          groupingConditions={groupingParameters.flatMap((p) => p.value)}
-          resultId={resultId}
-          type={type}
-          x={xAxis.value as AREA_DATASET_COLUMN}
-          y={yAxis.value as AREA_DATASET_COLUMN}
-        />
-      </div>
-    );
-  }
-
-  if (style === "line") {
-    const {
-      xAxis,
-      yAxis,
-      groupingParameters,
-      filterParameters,
-      yearParameter,
-      areaParameter: areaParameter,
-      groupingCalc,
-    } = pickArgsFromParameters(parameters, style);
-
-    if (!xAxis || !yAxis) {
-      return <div>パラメーターの値を正しく設定してください</div>;
-    }
-
-    if (type === "building") {
+    case style === "line" && unit === "building":
       return (
         <div>
-          <ChartLine
-            view={{
-              dataSetResultId: resultId,
-              unit: type,
-              style: "line",
-              title: "",
-              parameters,
-            }}
-          />
+          <ChartLine view={view} />
         </div>
       );
-    }
+    case style === "table" && unit === "building": {
+      const columns = parameters.find((p) => p.type === "column");
 
-    return (
-      <div>
-        <ChartLine
-          filterByAreas={areaParameter?.value}
-          filterByYear={{
-            startValue: yearParameter?.value?.start,
-            endValue: yearParameter?.value?.end,
-          }}
-          filterConditions={filterParameters.flatMap((p) => p.value)}
-          groupingCalc={groupingCalc?.value as "avg" | "sum"}
-          groupingConditions={groupingParameters.flatMap((p) => p.value)}
-          resultId={resultId}
-          type={type}
-          x={xAxis.value as AREA_DATASET_COLUMN}
-          y={yAxis.value as AREA_DATASET_COLUMN}
-        />
-      </div>
-    );
-  }
+      const yearParameter = parameters.find(
+        (p) => p.key === "year" && p.type === "filter",
+      );
 
-  if (style === "table") {
-    const columns = parameters.find((p) => p.type === "column");
+      const filterParameters = parameters.filter(
+        (p) => p.type === "filter" && p.key !== "year" && p.key !== "area",
+      );
 
-    const yearParameter = parameters.find(
-      (p) => p.key === "year" && p.type === "filter",
-    );
+      const areaParameter = parameters.find(
+        (p) => p.key === "area" && p.type === "filter",
+      );
 
-    const filterParameters = parameters.filter(
-      (p) => p.type === "filter" && p.key !== "year" && p.key !== "area",
-    );
+      if (!columns) {
+        return <div>パラメーターの値を正しく設定してください</div>;
+      }
 
-    const areaParameter = parameters.find(
-      (p) => p.key === "area" && p.type === "filter",
-    );
+      if (unit === "building") {
+        return (
+          <div>
+            <TableView
+              columns={columns.value.split(",") as BUILDING_DATASET_COLUMN[]}
+              filterByAreas={areaParameter?.value}
+              filterByYear={{
+                startValue: yearParameter?.value?.start,
+                endValue: yearParameter?.value?.end,
+              }}
+              filterConditions={filterParameters.flatMap((p) => p.value)}
+              resultId={dataSetResultId}
+              type={unit}
+            />
+          </div>
+        );
+      }
 
-    if (!columns) {
-      return <div>パラメーターの値を正しく設定してください</div>;
-    }
-
-    if (type === "building") {
       return (
         <div>
           <TableView
-            columns={columns.value.split(",") as BUILDING_DATASET_COLUMN[]}
+            columns={columns.value.split(",") as AREA_DATASET_COLUMN[]}
             filterByAreas={areaParameter?.value}
             filterByYear={{
               startValue: yearParameter?.value?.start,
               endValue: yearParameter?.value?.end,
             }}
             filterConditions={filterParameters.flatMap((p) => p.value)}
-            resultId={resultId}
-            type={type}
+            resultId={dataSetResultId}
+            type={unit}
           />
         </div>
       );
     }
+    case style === "map": {
+      const areas = parameters.find(
+        (p) => p.key === "area" && p.type === "filter",
+      )?.value;
 
-    return (
-      <div>
-        <TableView
-          columns={columns.value.split(",") as AREA_DATASET_COLUMN[]}
-          filterByAreas={areaParameter?.value}
-          filterByYear={{
-            startValue: yearParameter?.value?.start,
-            endValue: yearParameter?.value?.end,
-          }}
-          filterConditions={filterParameters.flatMap((p) => p.value)}
-          resultId={resultId}
-          type={type}
-        />
-      </div>
-    );
-  }
-
-  if (style === "map") {
-    const areas = parameters.find(
-      (p) => p.key === "area" && p.type === "filter",
-    )?.value;
-
-    return (
-      <div>
-        <Map areas={areas} dataSetResultId={resultId} type={type} />
-      </div>
-    );
+      return (
+        <div>
+          <Map areas={areas} dataSetResultId={dataSetResultId} type={unit} />
+        </div>
+      );
+    }
   }
 
   return <>未設定</>;
