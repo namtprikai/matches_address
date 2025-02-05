baded4d24214ae777618537037ad31b369cfeebb
ksyunnnn <is.syunsukekobashi@gmail.com>
2025-02-05 14:33:49 +0900

    利用しない棒グラフ:建物データ用の関数を仮実装

---
+++ app/src/bi-modules/api/_fetch-building-bar-chart-data.ts
@@ -0,0 +1,169 @@
+import { and, eq, gte, lte, ne, or, type SQL, sql } from "drizzle-orm";
+import { db } from "../../utils/db";
+import { type BarView } from "../interfaces/view";
+import { data_set_detail_buildings } from "../../schema";
+import { type FilterCondition } from "../interfaces/parameter";
+import { filterQueryBuilder } from "./filter-query-builder";
+import { conditionsToCaseQuery } from "./conditions-to-case-query";
+
+type Params = {
+  view: BarView;
+  pagination: {
+    limit: number;
+    offset: number;
+  };
+};
+
+type ReturnType = {
+  x: string;
+  y: number;
+  group?: unknown;
+}[];
+
+/**
+ * 現在棒グラフから建物データは参照していない
+ * 2025.02.05時点でこの関数動作は保証していない
+ */
+export const _fetchBuildingBarChartData = ({
+  view,
+  pagination: { limit, offset },
+}: Params): ReturnType => {
+  if (view.style !== "bar") {
+    throw new Error(
+      'このAPIは棒グラフ(style: "bar")のデータのみ対応しています',
+    );
+  }
+  if (view.unit !== "building") {
+    throw new Error(
+      'このAPIは建物単位(unit: "building")のデータのみ対応しています',
+    );
+  }
+
+  const { dataSetResultId } = view;
+
+  // パラメータの型安全な抽出
+  const xAxis = view.parameters.find((p) => p.key === "xAxis");
+  const yAxis = view.parameters.find((p) => p.key === "yAxis");
+  const yearFilter = view.parameters.find((p) => p.key === "year");
+  const areaFilter = view.parameters.find((p) => p.key === "area");
+  const groupConditions = view.parameters.filter((p) => p.type === "group");
+  const filterConditions = view.parameters.filter(
+    (p): p is FilterCondition /** startsWithが型推論しないため */ =>
+      p.key.startsWith("filter_"),
+  );
+  const groupAggregation = view.parameters.find(
+    (p) => p.type === "group_aggregation",
+  );
+
+  // 必須パラメータの検証
+  if (!dataSetResultId) {
+    throw new Error("dataSetResultIdは必須です");
+  }
+  if (!xAxis || !yAxis) {
+    throw new Error("X軸とY軸の設定は必須です");
+  }
+
+  // クエリのベース作成
+  const query = db
+    .select({
+      /** data_set_detail_buildingsのColumn名とそれぞれのvalueに定義された値が一致していることが前提でrawを利用 */
+      [xAxis.value]: sql.raw(`${xAxis.value}`).as(xAxis.value),
+      [yAxis.value]: sql.raw(`${yAxis.value}`).as(yAxis.value),
+    })
+    .from(data_set_detail_buildings)
+    .$dynamic();
+
+  const queryWheres: (SQL<unknown> | undefined)[] = [
+    eq(data_set_detail_buildings.data_set_result_id, dataSetResultId),
+  ];
+
+  /** 年のフィルタ */
+  if (yearFilter?.value.start) {
+    queryWheres.push(
+      gte(
+        data_set_detail_buildings.reference_date,
+        `${yearFilter.value.start}-01-01`,
+      ),
+    );
+  }
+  if (yearFilter?.value.end) {
+    queryWheres.push(
+      lte(
+        data_set_detail_buildings.reference_date,
+        `${yearFilter.value.end}-12-31`,
+      ),
+    );
+  }
+
+  /** 地域のフィルタ */
+  if (areaFilter?.value) {
+    queryWheres.push(
+      or(
+        ...(areaFilter.value ?? []).map((area) =>
+          eq(data_set_detail_buildings.area_group, area),
+        ),
+      ),
+    );
+  }
+
+  /** フィルタ詳細条件のフィルタ */
+  if (filterConditions) {
+    queryWheres.push(
+      and(...filterQueryBuilder({ conditions: filterConditions })),
+    );
+  }
+
+  /** 重複を排除する */
+  query
+    .groupBy(sql.raw(`${xAxis.value}`))
+    .having(sql.raw(`${xAxis.value} <> ''`));
+
+  const baseQuery = query.as("baseQuery");
+
+  /**
+   * グループ条件がある場合の処理
+   * - paginationは適用されない
+   * - @todo UIでは適用できそうになってるので修正が必要そう
+   */
+  if (groupConditions.length > 0) {
+    const GroupLabel = `group` as const;
+    const caseQuery = conditionsToCaseQuery(xAxis.value, groupConditions);
+
+    const groupQuery = db
+      .select({
+        [xAxis.value]: baseQuery[xAxis.value],
+        [yAxis.value]: baseQuery[yAxis.value],
+        [GroupLabel]: caseQuery.as("caseQuery"),
+      })
+      .from(baseQuery)
+      .as("GroupLabel");
+
+    const result = db
+      .select({
+        x: groupQuery[GroupLabel],
+        y: sql.raw(
+          `${groupAggregation?.value || "avg"}(${yAxis.value}) as ${yAxis.value}`,
+        ),
+        [GroupLabel]: groupQuery[GroupLabel],
+      })
+      .from(groupQuery)
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment -- @fixme 解決できないため
+      // @ts-ignore
+      .groupBy(groupQuery[GroupLabel])
+      .having(ne(groupQuery[GroupLabel], sql.raw("''")))
+      .all();
+    return result as ReturnType; /** @todo */
+  }
+
+  const result = db
+    .select({
+      x: baseQuery[xAxis.value],
+      y: baseQuery[yAxis.value],
+    })
+    .from(baseQuery)
+    .limit(limit)
+    .offset(offset)
+    .all();
+
+  return result as ReturnType; /** @todo */
+};
