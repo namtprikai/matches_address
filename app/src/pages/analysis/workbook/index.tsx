c1659e4ced85c5bd45e7c2a46d7e0098c142b229
ksyunnnn <is.syunsukekobashi@gmail.com>
2025-02-06 10:28:19 +0900

    不要コードの削除・lint対応

--- app/src/pages/analysis/workbook/index.tsx
+++ app/src/pages/analysis/workbook/index.tsx
@@ -5,7 +5,7 @@ import {
   BreadcrumbBase,
   BreadcrumbItem,
 } from "../../../components/ui/breadcrumb";
-import { ROUTES, withHash } from "../../../routes";
+import { ROUTES } from "../../../routes";
 
 const useStyles = makeStyles({
   root: {
