41703488629ee1fc989394243f98b34b48d1172b
ksyunnnn <is.syunsukekobashi@gmail.com>
2025-02-18 08:29:11 +0900

    不足columnの追加

---
+++ app/drizzle/0021_big_zaran.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `data_set_detail_buildings` ADD `max_age` integer;--> statement-breakpoint
+ALTER TABLE `data_set_detail_buildings` ADD `min_age` integer;--> statement-breakpoint
+ALTER TABLE `data_set_detail_buildings` ADD `change_ratio_water_usage` real;
