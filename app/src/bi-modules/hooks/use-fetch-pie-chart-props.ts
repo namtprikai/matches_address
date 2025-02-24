68b1a3f5adfd10e9bd15a24686eb1f3d899d280b
ksyunnnn <is.syunsukekobashi@gmail.com>
2025-02-25 00:15:38 +0900

    別ページ

--- app/src/bi-modules/hooks/use-fetch-pie-chart-props.ts
+++ app/src/bi-modules/hooks/use-fetch-pie-chart-props.ts
@@ -5,6 +5,7 @@ import {
   usePagination,
   type UsePaginationReturnType,
 } from "../../hooks/use-pagination";
+import { useIsLoading } from "../../hooks/use-is-loading";
 import { useChartProps } from "./use-chart-props";
 
 type Params = {
@@ -15,21 +16,31 @@ type ReturnType = {
   chartProps: ChartProps;
   refetch: () => Promise<void>;
   pagination: UsePaginationReturnType;
+  isLoading: boolean;
 };
 
 export const useFetchPieChartProps = ({ view }: Params): ReturnType => {
   const pagination = usePagination(100);
   const { chartProps, handleChartProps } = useChartProps();
+  const { isLoading, handleIsLoading } = useIsLoading({ init: true });
 
   const fetch = useCallback(async (): Promise<void> => {
-    const result = await window.ipcRenderer.invoke("fetchChartData", {
-      view,
-      pagination: {
-        limit: pagination.limitPerPage,
-        offset: pagination.limitPerPage * (pagination.page - 1),
-      },
-    });
-    handleChartProps(result);
+    try {
+      handleIsLoading(true);
+      const result = await window.ipcRenderer.invoke("fetchChartData", {
+        view,
+        pagination: {
+          limit: pagination.limitPerPage,
+          offset: pagination.limitPerPage * (pagination.page - 1),
+        },
+      });
+      handleChartProps(result);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      handleIsLoading(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps -- handleIsLoading を追加するよう指摘されるが、追加すると無限ループになるため無視 @fixme
   }, [pagination.limitPerPage, pagination.page, view, handleChartProps]);
 
   useEffect(() => {
@@ -40,5 +51,6 @@ export const useFetchPieChartProps = ({ view }: Params): ReturnType => {
     chartProps,
     refetch: fetch,
     pagination,
+    isLoading,
   };
 };
