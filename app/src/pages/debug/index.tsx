d43c6cd035fe70869401e660dc050d31bb54c9f1
ksyunnnn <is.syunsukekobashi@gmail.com>
2025-03-06 11:44:17 +0900

    開発者向けページを追加

--- app/src/pages/debug/index.tsx
+++ app/src/pages/debug/index.tsx
@@ -1,20 +1,45 @@
-import { makeStyles } from "@fluentui/react-components";
+import { makeStyles, tokens } from "@fluentui/react-components";
+import { BreadcrumbBase, BreadcrumbItem } from "../../components/ui/breadcrumb";
+import { ROUTES } from "../../routes";
 
 const useStyles = makeStyles({
-  flex: {
+  root: {
+    display: "grid",
+    gap: tokens.spacingVerticalXXL,
+  },
+  heading: {
+    fontSize: tokens.fontSizeBase500,
+    lineHeight: tokens.lineHeightBase600,
+  },
+  content: {
     display: "flex",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
     flexDirection: "column",
+    minHeight: "300px",
+    padding: `${tokens.spacingVerticalXXL} ${tokens.spacingHorizontalXXL}`,
+    gap: tokens.spacingVerticalXL,
   },
 });
 
 export function Debug(): JSX.Element {
   const styles = useStyles();
+
   return (
-    <div>
-      <h1>(開発用)</h1>
-      <div className={styles.flex}>
-        <a href="#">Go to home page</a>
-      </div>
+    <div className={styles.root}>
+      <BreadcrumbBase
+        breadcrumbItem={[
+          {
+            children: "開発者向け",
+            current: true,
+            href: ROUTES.DEBUG,
+          },
+        ].map((item) => (
+          <BreadcrumbItem key={item.href} {...item} />
+        ))}
+      />
+      <h2 className={styles.heading}>開発者向け</h2>
+      <div></div>
     </div>
   );
 }
