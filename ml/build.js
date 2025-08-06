a09d45d8ec35839f2525a8467d95e6af04258be0
rikaikhailv <106645885+rikaikhailv@users.noreply.github.com>
2025-08-06 17:20:33 +0700

    add lib sklearn when build exe (#1093)

--- ml/build.js
+++ ml/build.js
@@ -21,7 +21,7 @@ try {
 
     `poetry run pyinstaller --onefile --distpath ${DIST_DIR} --collect-all imblearn --collect-all memory_profiler --collect-all chardet --collect-all pandas --collect-all sklearn --collect-all lightgbm --collect-all numpy --collect-all optuna --collect-all seaborn --collect-all japanize_matplotlib --add-data="src${separator}src" --paths="${SRC_DIR}" --name IF002 ./async_tasks/IF002.py`,
 
-    `poetry run pyinstaller --onefile --distpath ${DIST_DIR} --collect-all chardet --collect-all pandas --collect-all geopandas --collect-all shapely --collect-all lightgbm --collect-all numpy --add-data="async_tasks${separator}async_tasks" --add-data="src${separator}src" --paths="${SRC_DIR}" --name IF003 ./async_tasks/IF003.py`,
+    `poetry run pyinstaller --onefile --distpath ${DIST_DIR} --collect-all chardet --collect-all pandas --collect-all geopandas --collect-all shapely --collect-all lightgbm --collect-all numpy --collect-all sklearn --add-data="async_tasks${separator}async_tasks" --add-data="src${separator}src" --paths="${SRC_DIR}" --name IF003 ./async_tasks/IF003.py`,
 
     `poetry run pyinstaller --onefile --distpath ${DIST_DIR} --collect-all chardet --collect-all pandas --collect-all geopandas --collect-all shapely --collect-all fiona --add-data="async_tasks${separator}async_tasks" --add-data="src${separator}src" --paths="${SRC_DIR}" --name IF004 ./async_tasks/IF004.py`,
   ];
