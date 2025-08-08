f71c64769dd7a9fa1561729c171673132d9c5f02
こばしゅん <is.syunsukekobashi@gmail.com>
2025-08-08 12:06:30 +0900

    feat: 推定不可物件の地図ポリゴンをグレー表示に変更 (#1109)

--- app/src/components/bi/map/map-component/_utils/create-add-layer-object.ts
+++ app/src/components/bi/map/map-component/_utils/create-add-layer-object.ts
@@ -1,5 +1,10 @@
 import { type AddLayerObject } from "maplibre-gl";
 
+const RED = "#C4314B";
+const YELLOW = "#FFA929";
+const GREEN = "#1B8C63";
+const GRAY = "#999999";
+
 export const createAddLayerObject = (
   layerId: string,
   predictedProbability: { medium: number; high: number },
@@ -12,11 +17,19 @@ export const createAddLayerObject = (
   paint: {
     "fill-color": [
       "case",
+      // 推定不可判定（outlier_flag, single_story_row_house_flag, matched_data_flagのいずれかが1）
+      [
+        "any",
+        ["==", ["get", "outlier_flag"], 1],
+        ["==", ["get", "single_story_row_house_flag"], 1],
+        ["==", ["get", "matched_data_flag"], 1],
+      ],
+      GRAY,
       [">=", ["get", "predicted_probability"], predictedProbability.high],
-      "#C4314B", // 赤 (80以上)
+      RED, // 赤 (80以上)
       [">=", ["get", "predicted_probability"], predictedProbability.medium],
-      "#FFA929", // 黄 (30以上80未満)
-      "#1B8C63", // 青 (30未満)
+      YELLOW, // 黄 (30以上80未満)
+      GREEN, // 緑 (30未満)
     ],
     "fill-opacity": [
       "case",
@@ -26,11 +39,19 @@ export const createAddLayerObject = (
     ],
     "fill-outline-color": [
       "case",
+      // 推定不可判定（outlier_flag, single_story_row_house_flag, matched_data_flagのいずれかが1）
+      [
+        "any",
+        ["==", ["get", "outlier_flag"], 1],
+        ["==", ["get", "single_story_row_house_flag"], 1],
+        ["==", ["get", "matched_data_flag"], 1],
+      ],
+      GRAY, // グレー（推定不可）
       [">=", ["get", "predicted_probability"], predictedProbability.high],
-      "#C4314B", // 赤 (80以上)
+      RED, // 赤 (80以上)
       [">=", ["get", "predicted_probability"], predictedProbability.medium],
-      "#FFA929", // 黄 (30以上80未満)
-      "#1B8C63", // 青 (30未満)
+      YELLOW, // 黄 (30以上80未満)
+      GREEN, // 緑 (30未満)
     ],
   },
 });
