cafa37f49c9f4b189b21a3a7b1f39d446d4f78dc
NamTPRikai <phuongnam.tran@rikai.technology>
2025-08-12 11:57:50 +0700

    fixed issue 1111

--- ml/build.bat
+++ ml/build.bat
@@ -8,7 +8,7 @@ rem Building IF002
 poetry run pyinstaller --onefile --noconsole --distpath ./dist --collect-all imblearn --collect-all memory_profiler --collect-all chardet --collect-all pandas --collect-all sklearn --collect-all lightgbm --collect-all numpy --collect-all optuna --collect-all seaborn --add-data "src:src" --paths="./src" --name IF002 ./async_tasks/IF002.py
 
 rem Building IF003
-poetry run pyinstaller --onefile --noconsole --distpath ./dist --collect-all chardet --collect-all pandas --collect-all geopandas --collect-all shapely --collect-all lightgbm --collect-all numpy --collect-all sklearn --add-data "async_tasks:async_tasks" --add-data "src:src" --paths="./src" --name IF003 ./async_tasks/IF003.py
+poetry run pyinstaller --onefile --noconsole --distpath ./dist --collect-all chardet --collect-all pandas --collect-all geopandas --collect-all shapely --collect-all lightgbm --collect-all numpy --collect-all sklearn --collect-all fiona --collect-all pyogrio --add-data "async_tasks:async_tasks" --add-data "src:src" --paths="./src" --name IF003 ./async_tasks/IF003.py
 
 rem Building IF004
 poetry run pyinstaller --onefile --noconsole --distpath ./dist --collect-all chardet --collect-all pandas --collect-all geopandas --collect-all shapely --collect-all fiona --add-data "async_tasks:async_tasks" --add-data "src:src" --paths="./src" --name IF004 ./async_tasks/IF004.py
